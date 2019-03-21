94f55379cb684b74710a31b898cba1ae261b16b7
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2019-03-21 13:30:49 -0300

    BBL-16 cross-org network layer in place

--- dev/5_network/network_vpc_peering_shared.tf
+++ dev/5_network/network_vpc_peering_shared.tf
@@ -1,12 +1,17 @@
-/*
 #
-# VPC Peering Connection
+# VPC Peering Connection with Share: Requester Side
+#
+# Consider 'destination_cidr_block' parameter will be the CIDR of the remote VPC/Subnets  (originator of the VPC
+# peering request) and 'route_table_id' the route table ID to add the destination route to.
 #
 resource "aws_vpc_peering_connection" "with_shared_vpc" {
     peer_owner_id = "${var.shared_account_id}"
     peer_vpc_id   = "${var.shared_vpc_id}"
     vpc_id        = "${module.vpc.vpc_id}"
     auto_accept   = false
+
+//    tags = "${merge(map("side", "accepter"), ${local.tags}"
+    tags = "${merge(map("Name", "requester-dev-to-shared"), local.tags)}"
 }
 
 #
@@ -21,4 +26,4 @@ resource "aws_route" "pub_route_table_1_to_shared_vpc" {
     route_table_id            = "${element(module.vpc.public_route_table_ids, 0)}"
     destination_cidr_block    = "${var.shared_vpc_cidr_block}"
     vpc_peering_connection_id = "${aws_vpc_peering_connection.with_shared_vpc.id}"
-}*/
+}
--- shared/5_network/dns_remote_zone_auth.tf
+++ shared/5_network/dns_remote_zone_auth.tf
@@ -1,12 +1,18 @@
-/*
-resource "null_resource" "associate_with_remote_zone" {
+#Reference Link: https://aws.amazon.com/premiumsupport/knowledge-center/private-hosted-zone-different-account/
+
+#============================================================#
+# HOSTED ZONE: aws.binbash.com.ar from Shared to Dev Account
+#============================================================#
+// eg:
+//aws route53 list-hosted-zones --profile ${var.profile}
+//aws route53 create-vpc-association-authorization  --hosted-zone-id Z2UPXXXXXXXXX --vpc VPCRegion=us-east-1,VPCId=vpc-6812de10 --profile 'profile-host-acct'
+//aws route53 list-vpc-association-authorizations   --hosted-zone-id Z2UPXXXXXXXXX                                              --profile 'profile-host-acct'
+//aws route53 associate-vpc-with-hosted-zone        --hosted-zone-id Z2UPXXXXXXXXX --vpc VPCRegion=us-east-1,VPCId=vpc-6812de10 --profile 'profile-remote-acct'
+//aws route53 delete-vpc-association-authorization  --hosted-zone-id Z2UPXXXXXXXXX --vpc VPCRegion=us-east-1,VPCId=vpc-6812de10 --profile 'profile-host-acct'
+
+
+resource "null_resource" "dns_private_hosted_zone_reate_vpc_association_auth" {
     provisioner "local-exec" {
-        command = "aws route53 associate-vpc-with-hosted-zone --profile ${var.profile} --hosted-zone-id ${var.dev_internal_zone_id} --vpc VPCRegion=${var.region},VPCId=${module.vpc.vpc_id}"
+        command = "aws route53 create-vpc-association-authorization --hosted-zone-id ${var.dev_internal_zone_id} --vpc VPCRegion=${var.region},VPCId=${var.dev_vpc_id} --profile ${var.profile}"
     }
 }
-
-resource "null_resource" "r53_to_appsprd_vpc" {
-    provisioner "local-exec" {
-        command = "aws route53 associate-vpc-with-hosted-zone --profile ${var.profile} --hosted-zone-id ${var.appsprd_internal_zone_id} --vpc VPCRegion=${var.region},VPCId=${module.vpc.vpc_id}"
-    }
-}*/
