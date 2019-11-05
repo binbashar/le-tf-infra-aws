b34db23c9af3622cbaebb946a907fcd609788cf7
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2019-11-05 19:25:05 -0300

    lambda nuke implemented except for vpc,s3 and dynamodb

--- dev/6_cloud-nuke/variables.tf
+++ dev/6_cloud-nuke/variables.tf
@@ -16,17 +16,12 @@ variable "project" {
   description = "Project Name"
 }
 
-variable "project_long" {
-  description = "Project Long Name"
-}
-
 variable "environment" {
   description = "Environment Name"
-  default     = "dev"
 }
 
 #===========================================#
-# Accounts                                  #
+# Lambda Nuke                               #
 #===========================================#
 variable "cloudwatch_schedule_expression" {
   description = "Define the aws cloudwatch event rule schedule expression, eg: everyday at 22hs cron(0 22 ? * MON-FRI *)"
@@ -37,23 +32,17 @@ variable "cloudwatch_schedule_expression" {
 variable "name" {
   description = "Define name to use for lambda function, cloudwatch event and iam role"
   type        = string
-  default     = "cloud_nuke_everything"
-}
-
-variable "custom_iam_role_arn" {
-  description = "Custom IAM role arn for the scheduling lambda"
-  type        = string
-  default     = null
+  default     = "cloud-nuke-everything"
 }
 
 variable "exclude_resources" {
-  description = "Define the resources that will not be destroyed, eg: key_pair,autoscaling,ebs,ec2,ecr,eks,elasticbeanstalk,elb,spot,dynamodb,elasticache,rds,redshift,cloudwatch,endpoint,efs,glacier,s3"
+  description = "Define the resources that will not be destroyed, eg: key_pair,eip,network_security,autoscaling,ebs,ec2,ecr,eks,elasticbeanstalk,elb,spot,dynamodb,elasticache,rds,redshift,cloudwatch,endpoint,efs,glacier,s3"
   type        = string
-  default     = "s3,dynamo,vpc"
+  default     = "s3,dynamodb,vpc"
 }
 
 variable "older_than" {
-  description = "Only destroy resources that were created before a certain period"
+  description = "Only destroy resources that were created before a certain period, eg: 0d, 1d, ... ,7d etc"
   type        = string
-  default     = "7d"
+  default     = "0d"
 }
