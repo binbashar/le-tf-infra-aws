b3a045dcaf8097e6734cc1a32d5a56381ae00672
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-01-08 08:21:49 -0300

    BBL-167 Dev Acct DNS layer segregated and migrated to tf0.12

---
+++ dev/6_dns/TODO/TODO.md
@@ -0,0 +1,2 @@
+Add Recommended Network ACL Rules for Your VPC
+https://docs.aws.amazon.com/vpc/latest/userguide/vpc-recommended-nacl-rules.html
---
+++ dev/6_dns/variables.tf
@@ -0,0 +1,64 @@
+#===========================================#
+# AWS Provider Settings                     #
+#===========================================#
+variable "region" {
+  description = "AWS Region"
+}
+
+variable "profile" {
+  description = "AWS Profile"
+}
+
+#===========================================#
+# Project Variables                         #
+#===========================================#
+variable "project" {
+  description = "Project Name"
+}
+
+variable "project_long" {
+  description = "Project Long Name"
+}
+
+variable "environment" {
+  description = "Environment Name"
+  default = "dev"
+}
+
+variable "bucket" {}
+variable "dynamodb_table" {}
+variable "encrypt" {}
+variable "cloudtrail_org_bucket" {}
+
+#===========================================#
+# Accounts                                  #
+#===========================================#
+variable "shared_account_id" {
+  description = "Account: Shared Resources"
+}
+
+#===========================================#
+# External Accounts Data                    #
+#===========================================#
+variable "security_account_id" {
+  description = "Security & Users Management Account ID"
+}
+
+variable "dev_account_id" {
+  description = "Dev/Stage Account ID"
+}
+
+#===========================================#
+# Networking                                #
+#===========================================#
+variable "vpc_shared_created" {
+  description = "true if Shared account VPC is created"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_dev_eks_created" {
+  description = "true if Dev account EKS VPC is created"
+  type        = bool
+  default     = false
+}
