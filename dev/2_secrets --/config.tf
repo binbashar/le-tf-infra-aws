ef7f74fd9018a758344866221cfa765e0999b51d
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-01-16 12:20:20 -0300

    BBL-167 dev/2_secrets layer fully tested and migrated to tf-0.12 - remember '--' represents that the layer is not currently created on AWS.

---
+++ dev/2_secrets --/config.tf
@@ -0,0 +1,19 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+  version = ">= 2.40"
+}
+
+#=============================#
+# Backend Config (partial)    #
+#=============================#
+terraform {
+  required_version = ">= 0.12.18"
+
+  backend "s3" {
+    key = "dev/secrets/terraform.tfstate"
+  }
+}
