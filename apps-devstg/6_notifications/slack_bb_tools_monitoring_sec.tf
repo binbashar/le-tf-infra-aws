35cf5c28f72c8595308fa8bc3593a1ae4c317ca0
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-02-04 11:21:19 -0300

    BBL-167 apps-devstg/6_notifications layer applied

---
+++ apps-devstg/6_notifications/slack_bb_tools_monitoring_sec.tf
@@ -0,0 +1,33 @@
+# Encrypt the URL, storing encryption here will show it in logs and in tfstate
+# https://www.terraform.io/docs/state/sensitive-data.html
+data "aws_kms_ciphertext" "slack_url_bb_monitoring_sec" {
+  plaintext = "https://hooks.slack.com/services/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"
+  key_id    = data.terraform_remote_state.security.outputs.aws_kms_key_arn
+}
+
+#============================#
+# AWS SNS -> Lambda -> Slack #
+#============================#
+# Set create_with_kms_key = true
+# when providing value of kms_key_arn to create required IAM policy which allows to decrypt using specified KMS key.
+module "notify_slack_bb_monitoring_sec" {
+  source = "git::git@github.com:binbashar/terraform-aws-notify-slack.git?ref=v2.10.0"
+
+  #
+  # Creation Flags
+  #
+  create               = true
+  create_sns_topic     = true
+
+  #
+  # Slack Webhook URL + Channel
+  #
+  slack_channel        = "bb-tools-monitoring-sec"
+  slack_username       = "aws-binbash-org"
+  slack_webhook_url    = data.aws_kms_ciphertext.slack_url_bb_monitoring_sec.ciphertext_blob
+
+  kms_key_arn          = data.terraform_remote_state.security.outputs.aws_kms_key_arn
+  lambda_function_name = "${var.project}-${var.environment}-notify-slack-bb-monitoring-sec"
+  sns_topic_name       = var.sns_topic_name_bb_monitoring_sec
+
+}
--- apps-devstg/6_notifications/variables.tf
+++ apps-devstg/6_notifications/variables.tf
@@ -1,17 +1,12 @@
 #
 # config/backend.config
 #
-#=============================#
-# AWS Provider Settings       #
-#=============================#
+#================================#
+# Terraform AWS Backend Settings #
+#================================#
 variable "region" {
   type        = string
   description = "AWS Region"
-}
-
-variable "region_secondary" {
-  type        = string
-  description = "AWS Scondary Region for HA"
 }
 
 variable "profile" {
@@ -57,8 +52,13 @@ variable "environment" {
 # config/extra.config
 #
 #=============================#
-# Accounts                    #
+# Accounts & Extra Vars       #
 #=============================#
+variable "region_secondary" {
+  type        = string
+  description = "AWS Scondary Region for HA"
+}
+
 variable "security_account_id" {
   type        = string
   description = "Account: Security & Users Management"
@@ -69,9 +69,14 @@ variable "shared_account_id" {
   description = "Account: Shared Resources"
 }
 
-variable "dev_account_id" {
+variable "appsdevstg_account_id" {
   type        = string
   description = "Account: Dev Modules & Libs"
+}
+
+variable "appsprd_account_id" {
+  type        = string
+  description = "Account: Prod Modules & Libs"
 }
 
 variable "cloudtrail_org_bucket" {
@@ -82,22 +87,18 @@ variable "cloudtrail_org_bucket" {
 #=============================#
 # Notifications               #
 #=============================#
-variable "sns_topic_name" {
+#
+# AWS SNS -> Lambda -> Slack: bb-tools-monitoring
+#
+variable "sns_topic_name_bb_monitoring" {
   description = ""
-  default     = "sns-topic-slack-notify"
+  default     = "sns-topic-slack-notify-monitoring-bb-monitoring"
 }
 
-variable "slack_webhook_url" {
+#
+# AWS SNS -> Lambda -> Slack: bb-tools-monitoring-sec
+#
+variable "sns_topic_name_bb_monitoring_sec" {
   description = ""
-  default     = "https://hooks.slack.com/services/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"
-}
-
-variable "slack_channel" {
-  description = ""
-  default     = "bb-tools-monitoring"
-}
-
-variable "slack_username" {
-  description = ""
-  default     = "aws-binbash-org"
+  default     = "sns-topic-slack-notify-monitoring-bb-monitoring-sec"
 }
