33f5ff4051cae50cfe828985bb9c095ebd244b87
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-02-04 12:13:05 -0300

    BBL-167 apps-prd/3_network placeholder layer added

---
+++ apps-prd/3_network --/config.tf
@@ -0,0 +1,49 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  version                 = "~> 2.46"
+  region                  = var.region
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/config"
+}
+
+#=============================#
+# Backend Config (partial)    #
+#=============================#
+terraform {
+  required_version = ">= 0.12.20"
+
+  backend "s3" {
+    key = "apps-prd/network/terraform.tfstate"
+  }
+}
+
+#=============================#
+# Data sources                #
+#=============================#
+
+#
+# data type from output for vpc
+#
+data "terraform_remote_state" "vpc-eks" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/k8s-eks/prerequisites/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "vpc-shared" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = "bb-shared-devops"
+    bucket  = "bb-shared-terraform-backend"
+    key     = "shared/network/terraform.tfstate"
+  }
+}
