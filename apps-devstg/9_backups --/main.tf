263e2cecfd7c43c63bb314e2cc6b9866f72062f6
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-02-04 11:26:03 -0300

    BBL-167 apps-devstg/9_store + apps-devstg/9_backups layers updated

---
+++ apps-devstg/9_backups --/main.tf
@@ -0,0 +1,14 @@
+#
+# CONSIDERATION: To be considered
+# https://trello.com/c/QOZ0aIGB/55-tf-modules-aws-backup-todo-issues
+#
+module "nightly_backups" {
+  source = "git::git@github.com:binbashar/terraform-aws-backup-by-tags.git?ref=0.1.1"
+  name   = "nightly_backups"
+  tags   = local.tags
+  selection_by_tags = {
+    "Backup" = "True"
+  }
+  schedule     = "cron(0 5 * * ? *)"
+  delete_after = 21
+}
