670562984db2ca5ae7c046bef3fed9702c37c949
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-02-04 15:04:46 -0300

    BBL-167 shared/4_security + shared/4_security_compliance layers updated

--- shared/4_security/keys.tf
+++ shared/4_security/keys.tf
@@ -1,4 +1,19 @@
 resource "aws_key_pair" "compute-ssh-key" {
-  key_name   = "${var.compute_ssh_key_name}"
-  public_key = "${var.compute_ssh_public_key}"
+  key_name   = var.compute_ssh_key_name
+  public_key = var.compute_ssh_public_key
+}
+
+module "kms_key" {
+  source = "git::git@github.com:binbashar/terraform-aws-kms-key.git?ref=0.4.0"
+
+  enabled                 = true
+  namespace               = var.project
+  stage                   = var.environment
+  name                    = var.kms_key_name
+  delimiter               = "-"
+  description             = "KMS key for Dev Account"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+  alias                   = "alias/${var.project}_${var.environment}_${var.kms_key_name}_key"
+
 }
--- shared/4_security/outputs.tf
+++ shared/4_security/outputs.tf
@@ -1,4 +1,29 @@
-# aws_key_pair name
+#
+# EC2 aws_key_pair name
+#
 output "aws_key_pair_name" {
-  value = "${aws_key_pair.compute-ssh-key.key_name}"
+  value = aws_key_pair.compute-ssh-key.key_name
+}
+
+#
+# KMS aws_kms_key outputs
+#
+output "aws_kms_key_arn" {
+  description = "Key ARN"
+  value       = module.kms_key.key_arn
+}
+
+output "aws_kms_key_id" {
+  description = "KMS Key ID"
+  value       = module.kms_key.key_id
+}
+
+output "aws_kms_key_alias_arn" {
+  description = "KMS Alias ARN"
+  value       = module.kms_key.alias_arn
+}
+
+output "aws_kms_key_alias_name" {
+  description = "KMS Alias name"
+  value       = module.kms_key.alias_name
 }
