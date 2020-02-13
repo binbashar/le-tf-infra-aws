64c5a9a18648982c0aeba6badf5229b2818d4ae0
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-02-13 11:23:30 -0300

    BBL-199 apps-devstg/11_ec2_flee_ansible layer added in order to test ansible playbook against a fleet of vms

---
+++ apps-devstg/11_ec2_fleet_ansible --/outputs.tf
@@ -0,0 +1,16 @@
+output "public_dns" {
+  description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
+  value       = module.ec2_ansible_fleet.public_dns
+}
+
+output "private_dns" {
+  description = "List of private DNS names assigned to the instances. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
+  value       = module.ec2_ansible_fleet.private_dns
+}
+
+output "private_ip" {
+  description = "List of private IP addresses assigned to the instances"
+  value       = module.ec2_ansible_fleet.private_ip
+}
+
+
