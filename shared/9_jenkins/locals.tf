fa080ae11cb275918ac33e2b3b2b07394f26c70d
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2020-04-20 10:09:02 -0300

    Create AWS resources for deploying a Jenkins server (#64)

---
+++ shared/9_jenkins/locals.tf
@@ -0,0 +1,15 @@
+locals {
+  tags = {
+    Name        = "${var.prefix}-${var.name}"
+    Terraform   = "true"
+    Environment = var.environment
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+echo "Hello Terraform! -> Installing pre-req packages here!"
+apt-get update
+apt-get install -y vim
+echo "DONE"
+EOF
+}
