35467a122017f542f888cab157ddc60503c5353b
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-05-13 13:47:02 -0300

    BBWW-34 apps-prd/4_security_certs adding AWS AMC related layer

---
+++ apps-prd/4_security_certs/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  private_domain = "aws.binbash.com.ar"
+  # Removing trailing dot from domain - just to be sure :)
+  private_domain_name = trimsuffix(local.private_domain, ".")
+
+  public_domain = "binbash.com.ar"
+  # Removing trailing dot from domain - just to be sure :)
+  public_domain_name = trimsuffix(local.public_domain, ".")
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.environment
+  }
+}
