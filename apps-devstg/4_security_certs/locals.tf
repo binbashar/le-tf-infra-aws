6d4a4bc9931a2c91c62e26d5536c648d1a2906a9
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-05-13 13:31:14 -0300

    BBWW-34 apps-devstg/4_security_certs adding AWS ACM layer

---
+++ apps-devstg/4_security_certs/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  domain = "aws.binbash.com.ar"
+
+  # Removing trailing dot from domain - just to be sure :)
+  domain_name = trimsuffix(local.domain, ".")
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.environment
+    Name = local.domain_name
+
+  }
+}
