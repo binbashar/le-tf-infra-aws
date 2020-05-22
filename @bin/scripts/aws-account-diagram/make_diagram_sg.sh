d4a3fa113702c4d8c78205a0e37971b91191a7c8
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-05-22 14:12:37 -0300

    BBL-299 improving make_diagram scripts

--- @bin/scripts/aws-account-diagram/make_diagram_sg.sh
+++ @bin/scripts/aws-account-diagram/make_diagram_sg.sh
@@ -6,29 +6,43 @@
 LOCAL_OS_USER_ID=$(id -u)
 LOCAL_OS_GROUP_ID=$(id -g)
 
+# AWS IAM profile and account
+# binbash-shared
 #
-# AWS auth files
+AWS_ACCOUNT_ENV="shared"
+AWS_IAM_PROFILE="bb-${AWS_ACCOUNT_ENV}-auditor-ci"
+AWS_ACCOUNT_NAME="bb-${AWS_ACCOUNT_ENV}"
+
+# binbash-apps-devstg
 #
+#AWS_ACCOUNT_ENV="apps-devstg"
+#AWS_IAM_PROFILE="bb-${AWS_ACCOUNT_ENV}-auditor-ci"
+#AWS_ACCOUNT_NAME="bb-${AWS_ACCOUNT_ENV}"
+
+# binbash-apps-prd
+#
+#AWS_ACCOUNT_ENV="apps-prd"
+#AWS_IAM_PROFILE="bb-${AWS_ACCOUNT_ENV}-auditor-ci"
+#AWS_ACCOUNT_NAME="bb-${AWS_ACCOUNT_ENV}"
+
+#==============================================================================#
+# AWS configure IAM credentials
+#==============================================================================#
 AWS_SHARED_CREDENTIALS_FILE_VAR="~/.aws/bb-le/credentials"
 export AWS_SHARED_CREDENTIALS_FILE=${AWS_SHARED_CREDENTIALS_FILE_VAR}
 AWS_CONFIG_FILE_VAR="~/.aws/bb-le/config"
 export AWS_CONFIG_FILE=${AWS_CONFIG_FILE_VAR}
 
-# AWS IAM profile and account
-#
-AWS_IAM_PROFILE="bb-shared-auditor-ci"
-AWS_ACCOUNT_NAME="bb-shared"
-
-# AWS configure IAM credentials
-#
 aws_access_key_id=$(aws configure get aws_access_key_id --profile ${AWS_IAM_PROFILE})
 aws_secret_access_key=$(aws configure get aws_secret_access_key --profile ${AWS_IAM_PROFILE})
 region=$(aws configure get default.region --profile ${AWS_IAM_PROFILE})
 aws_account=$(aws sts get-caller-identity --profile ${AWS_IAM_PROFILE} | jq -r .Account)
 
-#
+
+
+#==============================================================================#
 # AWS SecurityViz
-#
+#==============================================================================#
 docker run -it --rm \
     -p 0.0.0.0:8081:3000 \
     -v $(pwd)/data:/aws-security-viz \
