83ad19151756ecb3ff1b222747b1856b36ea4134
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-07-24 11:58:26 -0300

    BBL-209 activating apps-prd and apps-devstg notifications for tools-monitoring slack channel

---
+++ apps-devstg/base-network/notifications.tf
@@ -0,0 +1,9 @@
+module "vpc-natgw-notifications" {
+  source = "github.com/binbashar/terraform-aws-natgw-notifications.git?ref=v0.0.4"
+
+  alarm_suffix              = "${var.environment}-account"
+  alarm_period              = 3600
+  alarm_evaluation_periods  = 1
+  send_sns                  = true
+  sns_topic_name            = data.terraform_remote_state.notifications.outputs.sns_topic_name_bb_monitoring
+}
