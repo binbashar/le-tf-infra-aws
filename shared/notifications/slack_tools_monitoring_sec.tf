fcf183dae51974b9453399f347b62a1cfebd731b
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2020-07-24 20:36:41 -0300

    Rename notifications resources in all accounts to remove 'bb' (#110)

--- shared/notifications/slack_tools_monitoring_sec.tf
+++ shared/notifications/slack_tools_monitoring_sec.tf
@@ -1,7 +1,7 @@
 # Encrypt the URL, storing encryption here will show it in logs and in tfstate
 # https://www.terraform.io/docs/state/sensitive-data.html
-data "aws_kms_ciphertext" "slack_url_bb_monitoring" {
-  plaintext = "https://hooks.slack.com/services/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"
+data "aws_kms_ciphertext" "slack_url_monitoring_sec" {
+  plaintext = local.secrets.slack_webhook_monitoring_sec
   key_id    = data.terraform_remote_state.security.outputs.aws_kms_key_arn
 }
 
@@ -10,25 +10,25 @@ data "aws_kms_ciphertext" "slack_url_bb_monitoring" {
 #============================#
 # Set create_with_kms_key = true
 # when providing value of kms_key_arn to create required IAM policy which allows to decrypt using specified KMS key.
-module "notify_slack_bb_monitoring" {
+module "notify_slack_monitoring_sec" {
   source = "github.com/binbashar/terraform-aws-notify-slack.git?ref=v3.3.0"
 
   #
   # Creation Flags
   #
-  create           = false
-  create_sns_topic = false
+  create           = true
+  create_sns_topic = true
 
   #
   # Slack Webhook URL + Channel
   #
-  slack_channel     = "tools-monitoring"
+  slack_channel     = "tools-monitoring-sec"
   slack_username    = "aws-binbash-org"
-  slack_webhook_url = data.aws_kms_ciphertext.slack_url_bb_monitoring.ciphertext_blob
+  slack_webhook_url = data.aws_kms_ciphertext.slack_url_monitoring_sec.ciphertext_blob
 
   kms_key_arn          = data.terraform_remote_state.security.outputs.aws_kms_key_arn
-  lambda_function_name = "${var.project}-${var.environment}-notify-slack-bb-monitoring"
+  lambda_function_name = "${var.project}-${var.environment}-notify-slack-monitoring-sec"
   lambda_description   = "Lambda function which sends notifications to Slack"
   log_events           = false
-  sns_topic_name       = var.sns_topic_name_bb_monitoring
+  sns_topic_name       = var.sns_topic_name_monitoring_sec
 }
