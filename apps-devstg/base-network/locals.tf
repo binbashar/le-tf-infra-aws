b21aa73ccfd2c1b6fb3d8e9e3d9b2de3daf68ccc
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-07-27 10:02:58 -0300

    BBL-209 testing natgw notifications

--- apps-devstg/base-network/locals.tf
+++ apps-devstg/base-network/locals.tf
@@ -51,3 +51,100 @@ locals {
     "kubernetes.io/role/elb" : 1
   }
 }
+
+locals {
+  network_acls = {
+    default_inbound = [
+      {
+        rule_number = 900
+        rule_action = "allow"
+        from_port   = 1024
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+    ]
+
+    default_outbound = [
+      {
+        rule_number = 900
+        rule_action = "allow"
+        from_port   = 1024
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+    ]
+
+    private_inbound = [
+      {
+        rule_number = 100
+        rule_action = "allow"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 110
+        rule_action = "allow"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 120
+        rule_action = "allow"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 130
+        rule_action = "allow"
+        from_port   = 3389
+        to_port     = 3389
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+    ]
+
+    private_outbound = [
+      {
+        rule_number = 100
+        rule_action = "allow"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 110
+        rule_action = "allow"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 120
+        rule_action = "allow"
+        from_port   = 1433
+        to_port     = 1433
+        protocol    = "tcp"
+        cidr_block  = "10.0.100.0/22"
+      },
+      {
+        rule_number = 130
+        rule_action = "allow"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_block  = "10.0.100.0/22"
+      },
+    ]
+  }
+}
+
--- apps-devstg/base-network/network.tf
+++ apps-devstg/base-network/network.tf
@@ -11,7 +11,12 @@ module "vpc" {
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
 
-  enable_nat_gateway   = false
+  manage_default_network_acl    = true
+  #private_dedicated_network_acl = true // use dedicated network ACL for the private subnets.
+  #private_inbound_acl_rules     = "${concat(local.network_acls["default_inbound"], local.network_acls["private_inbound"])}"
+  #private_outbound_acl_rules    = "${concat(local.network_acls["default_outbound"], local.network_acls["private_outbound"])}"
+
+  enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_vpn_gateway   = false
