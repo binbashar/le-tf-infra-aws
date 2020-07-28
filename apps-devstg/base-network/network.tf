30cbd8e82c6696847b1ad4f11ab60697f1c14366
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-07-28 14:22:44 -0300

    BBL-209 apps-devstg/base-networks sintaxt improvement

--- apps-devstg/base-network/network.tf
+++ apps-devstg/base-network/network.tf
@@ -11,6 +11,11 @@ module "vpc" {
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
 
+  enable_nat_gateway   = false
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_vpn_gateway   = false
+
   manage_default_network_acl    = false
   public_dedicated_network_acl  = true // use dedicated network ACL for the public subnets.
   private_dedicated_network_acl = true // use dedicated network ACL for the private subnets.
@@ -18,11 +23,6 @@ module "vpc" {
   local.network_acls["default_inbound"],
   local.network_acls["private_inbound"],
   )
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_vpn_gateway   = false
 
   public_subnet_tags  = local.public_subnet_tags
   private_subnet_tags = local.private_subnet_tags
