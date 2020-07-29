3b992a5609d17bd3f7b1b8fe2f3ad649fd3b50a7
Diego OJ <diego.ojeda@binbash.com.ar>
2020-07-28 21:25:18 -0300

    Refactor AppsDevStg's security to use separate directories for separate concerns in order to avoid cross-references between them

--- apps-devstg/security-base/variables.tf
+++ apps-devstg/security-base/variables.tf
@@ -85,36 +85,3 @@ variable "appsprd_account_id" {
   type        = string
   description = "Account: Prod Modules & Libs"
 }
-
-#===========================================#
-# Security                                  #
-#===========================================#
-variable "compute_ssh_key_name" {
-  type        = string
-  description = "EC2 ssh public key name"
-  default     = "apps-devstg-default"
-}
-
-variable "compute_ssh_public_key" {
-  type        = string
-  description = "EC2 ssh public key"
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwqY8pH6XktrIOZ4JK2eaWg4QRkIkr2ua4IqfPhU+RPzCdBLCv1imX9kevX+dd0rplQAHibagouwLie99rEv1qR1Lt82jOXkBACdLCDaW5CGn2LTKFHN3Lm+oFRu9jzKRB6d2hm0qNuECvL1X2QAgbeGq5RDTwxVLg33l/EggpNbZZoh11w/UrSkvy2wYuYtLAN5oGj47+mvxpRvrcYK99zMOla6M6C5MrxllxaNcZXaO7cHZFLNFG5mbfJ/MdzHy9u46v3cf012UzhkrSkCqLSz2r2U25gKNWcOqmE0AMNW6qLBWmXnG+wUEBebX9v4KDRKfjbxpWJLQdr5CHav4l delivery@delivery-I7567"
-}
-
-variable "metric_namespace" {
-  type        = string
-  description = "A namespace for grouping all of the metrics together"
-  default     = "CISBenchmark"
-}
-
-variable "create_dashboard" {
-  type        = bool
-  description = "When true a dashboard that displays the statistics as a line graph will be created in CloudWatch"
-  default     = true
-}
-
-variable "kms_key_name" {
-  type        = string
-  description = "KMS key solution name, e.g. 'app' or 'jenkins'"
-  default     = "kms"
-}
