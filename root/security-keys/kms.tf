7350d8154fd7ac7b479e996c2551470f2fd751d7
Diego OJ <diego.ojeda@binbash.com.ar>
2020-07-29 13:00:51 -0300

    Ensure Root KMS module is consistent with the versions used in other accounts

--- root/security-keys/kms.tf
+++ root/security-keys/kms.tf
@@ -1,5 +1,5 @@
 module "kms_key" {
-  source = "github.com/binbashar/terraform-aws-kms-key.git?ref=0.4.0"
+  source = "github.com/binbashar/terraform-aws-kms-key.git?ref=0.5.0"
 
   enabled                 = true
   namespace               = var.project
