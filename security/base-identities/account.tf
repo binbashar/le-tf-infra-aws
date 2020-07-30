c3034b8cda820b5bbf9e92def5364373e44fb34f
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-07-30 10:49:22 -0300

    BBL-192 base-identities password length set to >= 30

--- security/base-identities/account.tf
+++ security/base-identities/account.tf
@@ -13,7 +13,7 @@ module "aws_iam_account_config" {
   # account password policy
   create_account_password_policy = true
   max_password_age               = 60
-  minimum_password_length        = 16
+  minimum_password_length        = 30
   require_numbers                = true
   require_lowercase_characters   = true
   require_symbols                = true
