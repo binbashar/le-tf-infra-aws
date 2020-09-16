eeb767fe28273171c397f6045bf48eb80864053c
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-09-16 10:22:39 -0300

    BBWW-57 | small binbash.com.ar update related to google search verification

--- shared/base-dns/binbash.com.ar/zone_public.binbash.com.ar.tf
+++ shared/base-dns/binbash.com.ar/zone_public.binbash.com.ar.tf
@@ -43,14 +43,6 @@ resource "aws_route53_record" "pub_CNAME_leverage_binbash_com_ar" {
 #
 # text records
 #
-resource "aws_route53_record" "aws_public_hosted_zone_1_TXT_record_1" {
-  zone_id = aws_route53_zone.aws_public_hosted_zone_1.id
-  name    = var.aws_public_hosted_zone_fqdn_1
-  type    = "TXT"
-  records = [var.aws_public_hosted_zone_1_text_record_1]
-  ttl     = 300
-}
-
 resource "aws_route53_record" "aws_public_hosted_zone_1_TXT_record_2" {
   zone_id = aws_route53_zone.aws_public_hosted_zone_1.id
   name    = var.aws_public_hosted_zone_1_text_record_2_name
@@ -72,5 +64,13 @@ resource "aws_route53_record" "aws_public_hosted_zone_1_TXT_record_4" {
   name    = var.aws_public_hosted_zone_1_text_record_4_name
   type    = "TXT"
   records = [var.aws_public_hosted_zone_1_text_record_4]
+  ttl     = 300
+}
+
+resource "aws_route53_record" "aws_public_hosted_zone_TXT_record_google_spf" {
+  zone_id = aws_route53_zone.aws_public_hosted_zone_1.id
+  name    = "binbash.com.ar"
+  type    = "TXT"
+  records = ["v=spf1 include:_spf.google.com ~all"]
   ttl     = 300
 }
