9342476275315f3e8d645874aee1a25d6e4113b3
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2020-09-17 09:46:43 -0300

    Import OrganizationAccountAccessRole in all accounts and enable MFA on it; also disable MFA on Security until the MFA script can support multi-profiles per layer (#137)

--- apps-prd/base-identities/roles.tf
+++ apps-prd/base-identities/roles.tf
@@ -109,3 +109,29 @@ module "iam_assumable_role_deploy_master" {
 
   tags = local.tags
 }
+
+#
+# Assumable Role Cross-Account: OrganizationAccountAccessRole
+#
+module "iam_assumable_role_oaar" {
+  source = "github.com/binbashar/terraform-aws-iam.git//modules/iam-assumable-role?ref=v2.20.0"
+
+  trusted_role_arns = [
+    "arn:aws:iam::${var.root_account_id}:root"
+  ]
+
+  create_role           = true
+  role_name             = "OrganizationAccountAccessRole"
+  admin_role_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  attach_admin_policy   = true
+  role_path             = "/"
+
+  #
+  # MFA setup
+  #
+  role_requires_mfa    = true
+  mfa_age              = 3600 # Maximum CLI/API session duration in seconds between 3600 and 43200
+  max_session_duration = 7200 # Max age of the session (in seconds) when assuming roles
+
+  tags = local.tags
+}
