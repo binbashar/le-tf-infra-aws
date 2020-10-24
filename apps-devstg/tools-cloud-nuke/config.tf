ba119086ef34f0a1148922036089db3b8d7a34bc
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-10-24 20:39:32 -0300

    BBL-438 | cross-layer minor improvements

--- apps-devstg/tools-cloud-nuke/config.tf
+++ apps-devstg/tools-cloud-nuke/config.tf
@@ -18,3 +18,21 @@ terraform {
     key = "apps-devstg/cloud-nuke/terraform.tfstate"
   }
 }
+
+#=============================#
+# Data sources                #
+#=============================#
+
+#
+# data type from output for security
+#
+data "terraform_remote_state" "keys" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/security-keys/terraform.tfstate"
+  }
+}
