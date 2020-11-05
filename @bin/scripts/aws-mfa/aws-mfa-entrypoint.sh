8633549bb59190ebd2d7954918c8099d0c677119
Diego OJ <diego.ojeda@binbash.com.ar>
2020-11-05 16:11:39 -0300

    Add support for multiple profiles on the MFA script

--- @bin/scripts/aws-mfa/aws-mfa-entrypoint.sh
+++ @bin/scripts/aws-mfa/aws-mfa-entrypoint.sh
@@ -11,7 +11,7 @@ set -o nounset
 
 # A simple logging function
 function log {
-    echo -e "[$(date +"%m-%d-%y %H:%M:%S")][DEBUG] $*"
+    echo -e "[$(date +"%m-%d-%y %H:%M:%S")] $*"
 }
 
 # Get the value of an entry in a config file
@@ -19,7 +19,7 @@ function get_config {
     local config_file=$1
     local config_key=$2
     local config_value=```
-grep -oEi "^$config_key.*=.*\"([a-zA-Z0-9\-]+)\"" $config_file \
+grep -oEi "^$config_key\s+=.*\"([a-zA-Z0-9\-]+)\"" $config_file \
 | grep -oEi "\".+\"" \
 | sed 's/\"//g'
 ```
@@ -45,101 +45,130 @@ aws configure get profile.$profile_name.$profile_key
 # Initialize variables
 # -----------------------------------------------------------------------------
 BACKEND_CONFIG_FILE=`printenv BACKEND_CONFIG_FILE`
+COMMON_CONFIG_FILE=`printenv COMMON_CONFIG_FILE`
 SRC_AWS_CONFIG_FILE=`printenv SRC_AWS_CONFIG_FILE`
 SRC_AWS_SHARED_CREDENTIALS_FILE=`printenv SRC_AWS_SHARED_CREDENTIALS_FILE`
 TF_AWS_CONFIG_FILE=`printenv AWS_CONFIG_FILE`
 TF_AWS_SHARED_CREDENTIALS_FILE=`printenv AWS_SHARED_CREDENTIALS_FILE`
+AWS_REGION="$(get_config $BACKEND_CONFIG_FILE region)"
 AWS_OUTPUT=json
 log "BACKEND_CONFIG_FILE: $BACKEND_CONFIG_FILE"
 log "SRC_AWS_CONFIG_FILE: $SRC_AWS_CONFIG_FILE"
 log "SRC_AWS_SHARED_CREDENTIALS_FILE: $SRC_AWS_SHARED_CREDENTIALS_FILE"
 log "TF_AWS_CONFIG_FILE: $TF_AWS_CONFIG_FILE"
 log "TF_AWS_SHARED_CREDENTIALS_FILE: $TF_AWS_SHARED_CREDENTIALS_FILE"
+log "AWS_REGION: $AWS_REGION"
 log "AWS_OUTPUT: $AWS_OUTPUT"
 
 
 # -----------------------------------------------------------------------------
-# Find the profile that Terraform will use from the Terraform config file
+# Parse all profiles from config.tf
 # -----------------------------------------------------------------------------
-TF_PROFILE_NAME="$(get_config $BACKEND_CONFIG_FILE profile)"
-log "PROFILE_NAME: $TF_PROFILE_NAME"
-AWS_REGION="$(get_config $BACKEND_CONFIG_FILE region)"
-log "AWS_REGION: $AWS_REGION"
+# Find any available profiles in config.tf
+RAW_PROFILES=()
+PARSED_PROFILES=`grep -E "^\s+profile" config.tf`
+while IFS= read -r line ; do
+    RAW_PROFILES+=(`echo $line | sed 's/ //g' | sed 's/[\"\$\{\}]//g'`)
+done <<< "$PARSED_PROFILES"
 
+# Replace placeholders in profiles
+PROFILES=()
+for i in "${RAW_PROFILES[@]}" ; do
+    PROFILE_VALUE="$(get_config $BACKEND_CONFIG_FILE profile)"
+    PROJECT_VALUE="$(get_config $COMMON_CONFIG_FILE project)"
+    TMP_PROFILE=`echo $i | sed "s/profile=//" | sed "s/var.profile/${PROFILE_VALUE}/" | sed "s/var.project/${PROJECT_VALUE}/"`
+    PROFILES+=("$TMP_PROFILE")
+done
 
-# -----------------------------------------------------------------------------
-# With that we can get the role, serial number and source profile from the AWS config file
-# -----------------------------------------------------------------------------
-MFA_ROLE_ARN="$(get_profile $SRC_AWS_CONFIG_FILE $SRC_AWS_SHARED_CREDENTIALS_FILE $TF_PROFILE_NAME role_arn)"
-log "MFA_ROLE_ARN: $MFA_ROLE_ARN"
-MFA_SERIAL_NUMBER="$(get_profile $SRC_AWS_CONFIG_FILE $SRC_AWS_SHARED_CREDENTIALS_FILE $TF_PROFILE_NAME mfa_serial)"
-log "MFA_SERIAL_NUMBER: $MFA_SERIAL_NUMBER"
-MFA_PROFILE_NAME="$(get_profile $SRC_AWS_CONFIG_FILE $SRC_AWS_SHARED_CREDENTIALS_FILE $TF_PROFILE_NAME source_profile)"
-log "MFA_PROFILE_NAME: $MFA_PROFILE_NAME"
-MFA_TOTP_KEY="$(get_profile $SRC_AWS_CONFIG_FILE $SRC_AWS_SHARED_CREDENTIALS_FILE $TF_PROFILE_NAME totp_key)"
-log "MFA_TOTP_KEY: $MFA_TOTP_KEY"
-
-
-#
-# Try and get and MFA key from the profile
-#
-if [[ $MFA_TOTP_KEY != "" ]]; then
-    echo "MFA_TOTP_KEY: $MFA_TOTP_KEY"
-    MFA_TOKEN_CODE=`oathtool --totp -b $MFA_TOTP_KEY`
-else
-    # If the MFA TOTP Key was not found, prompt the user for the MFA Token
-    MFA_TOKEN_CODE=```
-read -p 'Type in your token code: ' TOKEN_CODE
-echo $TOKEN_CODE
-```
+# De-duplicate profiles
+UNIQ_PROFILES=($(echo "${PROFILES[@]}" | tr ' ' '\n' | uniq | tr '\n' ' '))
+if [[ "${#UNIQ_PROFILES[@]}" -eq 0 ]]; then
+    log "ERROR: Unable to find any profiles in config.tf"
 fi
-log "MFA_TOKEN_CODE: $MFA_TOKEN_CODE"
 
 
 # -----------------------------------------------------------------------------
-# At this point we are ready to assume the role to generate the temporary credentials
+# Loop through PROFILES and do all of the following
 # -----------------------------------------------------------------------------
-MFA_ROLE_SESSION_NAME="$MFA_PROFILE_NAME-temp"
-MFA_DURATION=900
-MFA_ASSUME_ROLE_OUTPUT=```
-AWS_CONFIG_FILE=$SRC_AWS_CONFIG_FILE \
-AWS_SHARED_CREDENTIALS_FILE=$SRC_AWS_SHARED_CREDENTIALS_FILE \
-aws sts assume-role \
-  --role-arn $MFA_ROLE_ARN \
-  --serial-number $MFA_SERIAL_NUMBER \
-  --role-session-name $MFA_ROLE_SESSION_NAME \
-  --duration-seconds $MFA_DURATION \
-  --token-code $MFA_TOKEN_CODE \
-  --profile $MFA_PROFILE_NAME
-```
-# log "MFA_ASSUME_ROLE_OUTPUT: $MFA_ASSUME_ROLE_OUTPUT"
-TEMP_FILE=/tmp/mfa-tmp-credentials
-echo "$MFA_ASSUME_ROLE_OUTPUT" > $TEMP_FILE
+for i in "${UNIQ_PROFILES[@]}" ; do
+    log "CURRENT_PROFILE: $i"
 
+    # -----------------------------------------------------------------------------
+    # Get the role, serial number and source profile from the AWS config file
+    # -----------------------------------------------------------------------------
+    MFA_ROLE_ARN="$(get_profile $SRC_AWS_CONFIG_FILE $SRC_AWS_SHARED_CREDENTIALS_FILE $i role_arn)"
+    log "MFA_ROLE_ARN: $MFA_ROLE_ARN"
+    MFA_SERIAL_NUMBER="$(get_profile $SRC_AWS_CONFIG_FILE $SRC_AWS_SHARED_CREDENTIALS_FILE $i mfa_serial)"
+    log "MFA_SERIAL_NUMBER: $MFA_SERIAL_NUMBER"
+    MFA_PROFILE_NAME="$(get_profile $SRC_AWS_CONFIG_FILE $SRC_AWS_SHARED_CREDENTIALS_FILE $i source_profile)"
+    log "MFA_PROFILE_NAME: $MFA_PROFILE_NAME"
+    MFA_TOTP_KEY="$(get_profile $SRC_AWS_CONFIG_FILE $SRC_AWS_SHARED_CREDENTIALS_FILE $i totp_key)"
+    log "MFA_TOTP_KEY: $MFA_TOTP_KEY"
+    # Validate required fields
+    if [[ $MFA_ROLE_ARN == "" ]]; then log "[ERROR] Missing 'role_arn'" && exit 1; fi
+    if [[ $MFA_SERIAL_NUMBER == "" ]]; then log "[ERROR] Missing 'mfa_serial'" && exit 1; fi
+    if [[ $MFA_PROFILE_NAME == "" ]]; then log "[ERROR] Missing 'source_profile'" && exit 1; fi
 
-# -----------------------------------------------------------------------------
-# Parse id, secret and session from the output above
-# -----------------------------------------------------------------------------
-AWS_ACCESS_KEY_ID=`cat $TEMP_FILE | jq .Credentials.AccessKeyId | sed -e 's/"//g'`
-AWS_SECRET_ACCESS_KEY=`cat $TEMP_FILE | jq .Credentials.SecretAccessKey | sed -e 's/"//g'`
-AWS_SESSION_TOKEN=`cat $TEMP_FILE | jq .Credentials.SessionToken | sed -e 's/"//g'`
-log "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID:0:5}*************"
-log "AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY:0:5}*************"
-log "AWS_SESSION_TOKEN: ${AWS_SESSION_TOKEN:0:5}*************"
-rm $TEMP_FILE
+    # -----------------------------------------------------------------------------
+    # Try and get and MFA key from the profile
+    # -----------------------------------------------------------------------------
+    if [[ $MFA_TOTP_KEY != "" ]]; then
+        log "MFA_TOTP_KEY: $MFA_TOTP_KEY"
+        MFA_TOKEN_CODE=`oathtool --totp -b $MFA_TOTP_KEY`
+    else
+        # If the MFA TOTP Key was not found, prompt the user for the MFA Token
+        MFA_TOKEN_CODE=```
+    read -p 'Type in your token code: ' TOKEN_CODE
+    echo $TOKEN_CODE
+    ```
+    fi
+    log "MFA_TOKEN_CODE: $MFA_TOKEN_CODE"
 
+    # -----------------------------------------------------------------------------
+    # At this point we are ready to assume the role to generate the temporary credentials
+    # -----------------------------------------------------------------------------
+    MFA_ROLE_SESSION_NAME="$MFA_PROFILE_NAME-temp"
+    MFA_DURATION=900
+    MFA_ASSUME_ROLE_OUTPUT=```
+    AWS_CONFIG_FILE=$SRC_AWS_CONFIG_FILE \
+    AWS_SHARED_CREDENTIALS_FILE=$SRC_AWS_SHARED_CREDENTIALS_FILE \
+    aws sts assume-role \
+      --role-arn $MFA_ROLE_ARN \
+      --serial-number $MFA_SERIAL_NUMBER \
+      --role-session-name $MFA_ROLE_SESSION_NAME \
+      --duration-seconds $MFA_DURATION \
+      --token-code $MFA_TOKEN_CODE \
+      --profile $MFA_PROFILE_NAME
+    ```
+    # log "MFA_ASSUME_ROLE_OUTPUT: $MFA_ASSUME_ROLE_OUTPUT"
+    TEMP_FILE=/tmp/mfa-tmp-credentials
+    echo "$MFA_ASSUME_ROLE_OUTPUT" > $TEMP_FILE
 
-# -----------------------------------------------------------------------------
-# Create a profile block in the AWS credentials file using the credentials above
-# -----------------------------------------------------------------------------
-REPLACE_CREDENTIALS=```
-AWS_CONFIG_FILE=$TF_AWS_CONFIG_FILE; \
-AWS_SHARED_CREDENTIALS_FILE=$TF_AWS_SHARED_CREDENTIALS_FILE; \
-aws configure set profile.$TF_PROFILE_NAME.aws_access_key_id $AWS_ACCESS_KEY_ID; \
-aws configure set profile.$TF_PROFILE_NAME.aws_secret_access_key $AWS_SECRET_ACCESS_KEY; \
-aws configure set profile.$TF_PROFILE_NAME.aws_session_token $AWS_SESSION_TOKEN; \
-aws configure set region $AWS_REGION; \
-aws configure set output $AWS_OUTPUT
-```
+    # -----------------------------------------------------------------------------
+    # Parse id, secret and session from the output above
+    # -----------------------------------------------------------------------------
+    AWS_ACCESS_KEY_ID=`cat $TEMP_FILE | jq .Credentials.AccessKeyId | sed -e 's/"//g'`
+    AWS_SECRET_ACCESS_KEY=`cat $TEMP_FILE | jq .Credentials.SecretAccessKey | sed -e 's/"//g'`
+    AWS_SESSION_TOKEN=`cat $TEMP_FILE | jq .Credentials.SessionToken | sed -e 's/"//g'`
+    log "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID:0:4}**************"
+    log "AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY:0:4}**************"
+    log "AWS_SESSION_TOKEN: ${AWS_SESSION_TOKEN:0:4}**************"
+    rm $TEMP_FILE
 
+    # -----------------------------------------------------------------------------
+    # Create a profile block in the AWS credentials file using the credentials above
+    # -----------------------------------------------------------------------------
+    REPLACE_CREDENTIALS=```
+    AWS_CONFIG_FILE=$TF_AWS_CONFIG_FILE; \
+    AWS_SHARED_CREDENTIALS_FILE=$TF_AWS_SHARED_CREDENTIALS_FILE; \
+    aws configure set profile.$i.aws_access_key_id $AWS_ACCESS_KEY_ID; \
+    aws configure set profile.$i.aws_secret_access_key $AWS_SECRET_ACCESS_KEY; \
+    aws configure set profile.$i.aws_session_token $AWS_SESSION_TOKEN; \
+    aws configure set region $AWS_REGION; \
+    aws configure set output $AWS_OUTPUT
+    ```
+
+done
+
+# Delegate execution to the given process
 exec "$@"
