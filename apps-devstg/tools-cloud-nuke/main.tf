889bb06fea188344e7e769f0fd0e1bfad85c4394
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-12-19 09:21:46 -0300

    BBL-453 | apps-devstg/tools-cloud-nuke layer updated to its latest stable module version

--- apps-devstg/tools-cloud-nuke/main.tf
+++ apps-devstg/tools-cloud-nuke/main.tf
@@ -1,5 +1,5 @@
 module "nuke_everything_daily_midnight" {
-  source = "github.com/binbashar/terraform-aws-lambda-nuke.git?ref=2.9.0"
+  source = "github.com/binbashar/terraform-aws-lambda-nuke.git?ref=2.10.0"
 
   # Define name to use for lambda function, cloudwatch event and iam role"
   name        = "${var.project}-${var.environment}-cloud-nuke-everything"
