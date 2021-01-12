8a44895d78e77d6361612d6551504a80143d95ee
OJ <diego.ojeda@binbash.com.ar>
2021-01-12 17:48:52 -0300

    Create a layer for EKS cluster

---
+++ apps-devstg/k8s-eks/cluster/config.tf
@@ -0,0 +1,61 @@
+#
+# Providers
+#
+provider "aws" {
+  version                 = "~> 3.2"
+  region                  = var.region
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/${var.project}/config"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.13"
+}
+
+#
+# Backend Config (partial)
+#
+terraform {
+  required_version = ">= 0.12.28"
+
+  backend "s3" {
+    key = "apps-devstg/k8s-eks/cluster/terraform.tfstate"
+  }
+}
+
+#
+# Data Sources
+#
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "terraform_remote_state" "shared-eks-vpc" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "apps-devstg/k8s-eks/vpc/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "shared-vpc" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = "${var.project}-shared-devops"
+    bucket  = "${var.project}-shared-terraform-backend"
+    key     = "shared/network/terraform.tfstate"
+  }
+}
---
+++ apps-devstg/k8s-eks/cluster/eks-workers-managed.tf
@@ -0,0 +1,86 @@
+module "eks" {
+  source = "github.com/binbashar/terraform-aws-eks.git?ref=v13.2.1"
+
+  create_eks      = true
+  cluster_name    = data.terraform_remote_state.shared-eks-vpc.outputs.cluster_name
+  cluster_version = var.cluster_version
+
+  #
+  # Network configurations
+  #
+  vpc_id  = data.terraform_remote_state.shared-eks-vpc.outputs.vpc_id
+  subnets = data.terraform_remote_state.shared-eks-vpc.outputs.private_subnets
+
+  #
+  # Security: public vs private access (and private access rules)
+  #
+  cluster_endpoint_private_access = var.cluster_endpoint_private_access
+  cluster_endpoint_public_access  = var.cluster_endpoint_public_access
+
+  cluster_create_endpoint_private_access_sg_rule = var.cluster_create_endpoint_private_access_sg_rule
+  cluster_endpoint_private_access_cidrs = [
+    data.terraform_remote_state.shared-vpc.outputs.vpc_cidr_block
+  ]
+
+  #
+  # Managed Nodes Default Settings
+  #
+  node_groups_defaults = {
+    # Managed Nodes cannot specify custom AMIs, only use the ones allowed by EKS
+    ami_type  = "AL2_x86_64"
+    disk_size = 50
+  }
+
+  #
+  # List of Managed Node Groups
+  #
+  node_groups = {
+    sample = {
+      name             = "${data.terraform_remote_state.shared-eks-vpc.outputs.cluster_name}-sample"
+      desired_capacity = 1
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t2.medium"
+      k8s_labels    = local.tags
+    }
+  }
+
+  #
+  # Auth: Kubeconfig
+  #
+  kubeconfig_name                              = var.kubeconfig_name
+  write_kubeconfig                             = var.write_kubeconfig
+  config_output_path                           = var.config_output_path
+  kubeconfig_aws_authenticator_additional_args = ["--cache"]
+  kubeconfig_aws_authenticator_env_variables = {
+    AWS_PROFILE                 = var.profile,
+    AWS_CONFIG_FILE             = "/Users/OJ/.aws/${var.project}/config",
+    AWS_SHARED_CREDENTIALS_FILE = "/Users/OJ/.aws/${var.project}/credentials"
+  }
+
+  #
+  # Auth: aws-iam-authenticator
+  #
+  manage_aws_auth = var.manage_aws_auth
+  map_roles       = var.map_roles
+  map_accounts    = var.map_accounts
+  map_users       = var.map_users
+
+  #
+  # Logging: which log types should be enabled and how long they should be kept for
+  #
+  cluster_enabled_log_types = [
+    # "api",
+    # "audit",
+    # "authenticator",
+    # "controllerManager",
+    # "scheduler"
+  ]
+  cluster_log_retention_in_days = 7
+
+  #
+  # Tags
+  #
+  tags = local.tags
+}
