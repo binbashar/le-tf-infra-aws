93055e6a74dd7e0e55f8a81bf1c000b2dd59c9e4
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2021-01-21 10:21:55 -0300

    BBL-24 | removing shared/base-network-integrations layer since we can't route traffic from other vpcs in the org (consider AWS TGW for this scenario)

--- shared/base-network-integrations/vault-hvn/network_vpc_flow_logs.tf
+++
@@ -1,7 +0,0 @@
-module "vpc_flow_logs" {
-  source = "github.com/binbashar/terraform-aws-vpc-flowlogs.git?ref=v1.0.8"
-
-  vpc_id             = module.vpc.vpc_id
-  bucket_name_prefix = "${var.project}-${var.environment}"
-  tags               = local.tags
-}
