05b466b65cd8887e7ea22378088a7c6ba5af38e1
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2021-02-24 12:22:01 -0300

    Update build script to restrict which tasks can be run from root or account paths (#187)

--- build.py
+++ build.py
@@ -3,32 +3,43 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 # Import leverage libraries
 from leverage import task
+from leverage import path
 # Import local libraries
 from _lib import terraform
 
 @task()
+def checkdir():
+    '''Check if current directory can execute certain tasks'''
+    if path.get_working_path() == path.get_root_path():
+        print("This task cannot be run from root path")
+        sys.exit(0)
+    elif path.get_working_path() == path.get_account_path():
+        print("This task cannot be run from account path")
+        sys.exit(0)
+
+@task(checkdir)
 def init(*args):
     '''Initialize Terraform in this layer.'''
     terraform.init(list(args))
     terraform.change_terraform_dir_ownership()
 
-@task()
+@task(checkdir)
 def plan(*args):
     '''Generate a Terraform execution plan for this layer.'''
     terraform.plan(list(args))
 
-@task()
+@task(checkdir)
 def apply(*args):
     '''Build or change the Terraform infrastructre in this layer.'''
     terraform.apply(list(args))
     terraform.change_terraform_dir_ownership()
 
 @task()
-def output():
+def output(checkdir):
     '''Show all terraform output variables of this layer.'''
     terraform.output()
 
-@task()
+@task(checkdir)
 def destroy(*args):
     '''Destroy terraform infrastructure in this layer.'''
     terraform.destroy(list(args))
@@ -38,7 +49,7 @@ def shell():
     '''Open a shell into the Terraform container in this layer.'''
     terraform.shell()
 
-@task()
+@task(checkdir)
 def version():
     '''Print terraform version.'''
     terraform.version()
@@ -53,17 +64,17 @@ def format_check():
     '''Check if Terraform files do not meet the canonical format.'''
     terraform.format_check()
 
-@task()
+@task(checkdir)
 def decrypt():
     '''Decrypt secrets.tf file.'''
     os.system("ansible-vault decrypt --output secrets.dec.tf secrets.enc")
 
-@task()
+@task(checkdir)
 def encrypt():
     '''Encrypt secrets.dec.tf file.'''
     os.system("ansible-vault encrypt --output secrets.enc secrets.dec.tf && rm -rf secrets.dec.tf")
 
-@task()
+@task(checkdir)
 def validate_layout():
     '''Validate the layout convention of this Terraform layer.'''
     return os.system("../../@bin/scripts/validate-terraform-layout.sh")
