2ce66792e90acc670837975f441e063b7b67f0b9
Diego OJ <diego.ojeda@binbash.com.ar>
2021-03-12 10:24:49 -0300

    Create RDS Aurora

---
+++ apps-devstg/databases-aurora/config.tf
@@ -0,0 +1,65 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  region                  = var.region
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/${var.project}/config"
+}
+
+provider "vault" {
+  address = var.vault_address
+  token   = var.vault_token
+}
+
+#=============================#
+# Backend Config (partial)    #
+#=============================#
+terraform {
+  required_version = ">= 0.14.4"
+
+  backend "s3" {
+    key = "apps-devstg/databases-aurora/terraform.tfstate"
+  }
+
+  required_providers {
+    aws = ">= 3.8"
+  }
+}
+
+#=============================#
+# Data sources                #
+#=============================#
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/network/terraform.tfstate"
+  }
+}
+
+
+data "terraform_remote_state" "eks_vpc" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/k8s-eks/network/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "shared_vpc" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = "${var.project}-shared-devops"
+    bucket  = "${var.project}-shared-terraform-backend"
+    key     = "shared/network/terraform.tfstate"
+  }
+}
