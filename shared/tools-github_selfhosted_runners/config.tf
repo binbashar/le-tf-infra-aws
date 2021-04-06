053e81ba17cee039be8504a283b99c7353b816e8
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2021-04-06 10:29:19 -0300

    Create self-hosted Github Runners (#198)
    * Create Self-hosted Github Runners
    
    * Fix format
    
    * Remove the source code of the Lambda's used by Github Runners module as it only needs the zip files in lambdas-download
    
    * Pretty format json files

---
+++ shared/tools-github_selfhosted_runners/config.tf
@@ -0,0 +1,47 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+}
+
+#=============================#
+# Backend Config (partial)    #
+#=============================#
+terraform {
+  required_version = ">= 0.13.2"
+
+  required_providers {
+    aws = "~> 3.27"
+  }
+
+  backend "s3" {
+    key = "shared/github-selfhosted-runners/terraform.tfstate"
+  }
+}
+
+#=============================#
+# Data sources                #
+#=============================#
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/network/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "keys" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/security-keys/terraform.tfstate"
+  }
+}
