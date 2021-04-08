33a51d3b93d8cfd041142d7abe2f6253ee5881fa
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2021-04-08 09:21:53 -0300

    BBL-478-479 | Adding new collaborator users to aws security account withing DevOps group (DevOps role assumable cross-org)

--- security/base-identities/outputs.tf
+++ security/base-identities/outputs.tf
@@ -1,6 +1,17 @@
 #
 # users.tf sensitive data output (alphabetically ordered)
 #
+output "user_angelo_fenoglio_name" {
+  description = "The user's name"
+  value       = module.user_angelo_fenoglio.this_iam_user_name
+}
+
+/*output "user_angelo_fenoglio_login_profile_encrypted_password" {
+  description = "The encrypted password, base64 encoded"
+  value       = module.user_angelo_fenoglio.this_iam_user_login_profile_encrypted_password
+  sensitive   = true
+}*/
+
 output "user_diego_ojeda_name" {
   description = "The user's name"
   value       = module.user_diego_ojeda.this_iam_user_name
@@ -20,6 +31,17 @@ output "user_exequiel_barrirero_name" {
 /*output "user_exequiel_barrirero_login_profile_encrypted_password" {
   description = "The encrypted password, base64 encoded"
   value       = module.user_exequiel_barrirero.this_iam_user_login_profile_encrypted_password
+  sensitive   = true
+}*/
+
+output "user_luis_gallardo_name" {
+  description = "The user's name"
+  value       = module.user_luis_gallardo.this_iam_user_name
+}
+
+/*output "user_luis_gallardo_login_profile_encrypted_password" {
+  description = "The encrypted password, base64 encoded"
+  value       = module.user_luis_gallardo.this_iam_user_login_profile_encrypted_password
   sensitive   = true
 }*/
 
