f0099f69ffa167910e2bf483b3f9f14bca548042
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2021-04-10 19:37:46 -0300

    Disable managing of AWS Auth in EKS cluster layer to favor the managi… (#203)
    * Disable managing of AWS Auth in EKS cluster layer to favor the managing of it in k8s-resources layer
    
    * Fix format

---
+++ apps-devstg/k8s-eks/k8s-resources/config.tf
@@ -0,0 +1,52 @@
+#
+# Providers
+#
+provider "aws" {
+  region                  = var.region
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/${var.project}/config"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+#
+# Backend Config (partial)
+#
+terraform {
+  required_version = ">= 0.14.4"
+
+  required_providers {
+    aws        = "~> 3.28"
+    kubernetes = "~> 2.0.2"
+  }
+
+  backend "s3" {
+    key = "apps-devstg/k8s-eks/k8s-resources/terraform.tfstate"
+  }
+}
+
+#
+# Data Sources
+#
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks-cluster.outputs.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks-cluster.outputs.cluster_id
+}
+
+data "terraform_remote_state" "eks-cluster" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "apps-devstg/k8s-eks/cluster/terraform.tfstate"
+  }
+}
