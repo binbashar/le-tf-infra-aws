b8c0077b286ee1a7f7d395e0a509ffafb32deec1
Luis M. Gallardo D <lgallard@gmail.com>
2021-04-25 20:25:20 -0300

    Fix metallb enabled var

--- apps-devstg/k8s-kind/k8s-resources/metallb.tf
+++ apps-devstg/k8s-kind/k8s-resources/metallb.tf
@@ -2,7 +2,7 @@
 # MetalLB for kind
 #------------------------------------------------------------------------------
 resource "helm_release" "metallb" {
-  count      = var.kind["metallb"] ? 1 : 0
+  count      = var.enable_metallb ? 1 : 0
   name       = "metallb"
   namespace  = kubernetes_namespace.metallb.id
   repository = "https://charts.bitnami.com/bitnami"
