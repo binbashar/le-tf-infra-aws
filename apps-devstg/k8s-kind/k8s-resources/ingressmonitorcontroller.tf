0617bd8c278783f7f016721d5b6a820afb22c236
Luis M. Gallardo D <lgallard@gmail.com>
2021-04-26 14:00:36 -0300

    Add ingressmonitorcontroller chart

--- apps-devstg/k8s-kind/k8s-resources/ingressmonitorcontroller.tf
+++ apps-devstg/k8s-kind/k8s-resources/ingressmonitorcontroller.tf
@@ -2,11 +2,9 @@
 # IngressMonitorController
 #------------------------------------------------------------------------------
 resource "helm_release" "ingressmonitorcontroller" {
-  count     = var.enable_ingressmonitorcontroller ? 1 : 0
-  name      = "ingressmonitorcontroller"
-  namespace = kubernetes_namespace.metallb.id
-  #repository = "https://github.com/binbashar/helm-charts"
-  repository = "/home/lgallard/git/binbash/helm-charts/charts/"
+  count      = var.enable_ingressmonitorcontroller ? 1 : 0
+  name       = "ingressmonitorcontroller"
+  repository = "https://binbashar.github.io/helm-charts/"
   chart      = "ingressmonitorcontroller"
   version    = "v2.0.14"
   values     = [file("chart-values/ingressmonitorcontroller.yaml")]
