1494f80202e0816f841b16585c1e21e9b71c51bc
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2021-04-30 08:59:43 -0300

    BBL-192 | shared/tools-managedeskibana referencing to the Binbash Leverage forked module by convention

--- shared/tools-managedeskibana/main.tf
+++ shared/tools-managedeskibana/main.tf
@@ -1,5 +1,5 @@
 module "managed_elasticsearch_kibana" {
-  source = "github.com/lgallard/terraform-aws-elasticsearch?ref=0.9.1"
+  source = "github.com/binbashar/terraform-aws-elasticsearch?ref=0.9.1"
 
   # Domain (cluster) name and ElasticSearch version
   domain_name           = local.domain_name
