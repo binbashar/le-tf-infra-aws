73dfaaf0063359ebfeb99861b46e4aecf8d539c9
Diego OJ <diego.ojeda@binbash.com.ar>
2021-05-03 22:31:41 -0300

    Add fluentd to k8s-kind layer and update demoapps layer to enable fluentd through Kokuwa's chart which is feature rich and more updated

--- apps-devstg/k8s-eks-demoapps/k8s-resources/monitoring_logging.tf
+++ apps-devstg/k8s-eks-demoapps/k8s-resources/monitoring_logging.tf
@@ -12,26 +12,26 @@
 #------------------------------------------------------------------------------
 # fluentd: collect cluster logs and ship them to ElasticSearch
 #------------------------------------------------------------------------------
-resource "helm_release" "fluentd" {
-  count      = var.enable_logging ? 1 : 0
-  name       = "fluentd"
-  namespace  = kubernetes_namespace.monitoring.id
-  repository = "https://binbashar.github.io/helm-charts/"
-  chart      = "fluentd-daemonset"
-  version    = "0.1.0"
-  values     = [file("chart-values/fluentd-daemonset.yaml")]
-}
+# resource "helm_release" "fluentd" {
+#   count      = var.enable_logging ? 1 : 0
+#   name       = "fluentd"
+#   namespace  = kubernetes_namespace.monitoring.id
+#   repository = "https://binbashar.github.io/helm-charts/"
+#   chart      = "fluentd-daemonset"
+#   version    = "0.1.0"
+#   values     = [file("chart-values/fluentd-daemonset.yaml")]
+# }
 
 #------------------------------------------------------------------------------
 # fluentd: this one uses a different chart which support many more features,
 #          however it was not verified to be a working solution yet
 #------------------------------------------------------------------------------
-# resource "helm_release" "fluentd" {
-#   count      = var.enable_logging ? 1 : 0
-#   name       = "fluentd"
-#   namespace  = kubernetes_namespace.monitoring.id
-#   repository = "https://kokuwaio.github.io/helm-charts"
-#   chart      = "fluentd-elasticsearch"
-#   version    = "11.10.0"
-#   values     = [file("chart-values/fluentd-elasticsearch.yaml")]
-# }
+resource "helm_release" "fluentd" {
+  count      = var.enable_logging ? 1 : 0
+  name       = "fluentd"
+  namespace  = kubernetes_namespace.monitoring.id
+  repository = "https://kokuwaio.github.io/helm-charts"
+  chart      = "fluentd-elasticsearch"
+  version    = "11.10.0"
+  values     = [file("chart-values/fluentd-elasticsearch.yaml")]
+}
