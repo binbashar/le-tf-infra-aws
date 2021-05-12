cac84c701054dbfbbd2dec230ffe9898a93679e0
Luis M. Gallardo D <lgallard@gmail.com>
2021-05-11 22:37:15 -0300

    Replicate demoapss files in k8-eks layer

--- apps-devstg/k8s-eks/k8s-resources/variables.tf
+++ apps-devstg/k8s-eks/k8s-resources/variables.tf
@@ -92,3 +92,94 @@ variable "vault_token" {
 variable "vault_address" {
   type = string
 }
+
+#=============================#
+# Layer Flags                 #
+#=============================#
+variable "enable_private_ingress" {
+  type    = bool
+  default = false
+}
+
+variable "enable_public_ingress" {
+  type    = bool
+  default = false
+}
+
+variable "enable_private_dns_sync" {
+  type    = bool
+  default = false
+}
+
+variable "enable_public_dns_sync" {
+  type    = bool
+  default = false
+}
+
+variable "enable_prometheus_dependencies" {
+  type    = bool
+  default = false
+}
+
+variable "enable_grafana_dependencies" {
+  type    = bool
+  default = false
+}
+
+variable "enable_cert_manager" {
+  type    = bool
+  default = false
+}
+
+variable "enable_vault" {
+  type    = bool
+  default = false
+}
+
+variable "enable_cicd" {
+  type    = bool
+  default = false
+}
+
+variable "enable_kubernetes_dashboard" {
+  type    = bool
+  default = false
+}
+
+variable "enable_scaling" {
+  type    = bool
+  default = false
+}
+
+variable "enable_gatus" {
+  type    = bool
+  default = false
+}
+
+variable "enable_logging" {
+  type    = bool
+  default = false
+}
+
+variable "enable_ingressmonitorcontroller" {
+  type    = bool
+  default = false
+}
+
+variable "kubernetes_dashboard_ingress_class" {
+  type    = string
+  default = "ingress-nginx-private"
+}
+
+variable "kubernetes_dashboard_hosts" {
+  type    = string
+  default = "kubernetes-dashboard.devstg.aws.binbash.com.ar"
+}
+
+#==================================#
+# Ingress Monitor Controller (IMC) #
+#==================================#
+variable "imc" {
+  type    = any
+  default = {}
+}
