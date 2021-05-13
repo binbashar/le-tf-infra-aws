337859a7af1fd8d105a93906d1bcf1a90a379192
Luis M. Gallardo D <lgallard@gmail.com>
2021-05-13 11:16:40 -0300

    Add external-dns role & policies for k8s-eks

--- shared/k8s-eks/identities/roles.tf
+++ shared/k8s-eks/identities/roles.tf
@@ -43,3 +43,26 @@ module "role_external_dns_private" {
     Purpose = "eks-oidc"
   }
 }
+#
+#
+# Role: external-dns (public) for EKS OIDC -- Enable or update upon cluster creation.
+#
+module "role_external_dns_public" {
+  source = "github.com/binbashar/terraform-aws-iam.git//modules/iam-assumable-role-with-oidc?ref=v3.8.0"
+
+  create_role  = true
+  role_name    = "external-dns-public"
+  provider_url = replace(data.terraform_remote_state.apps-devstg-eks-cluster.outputs.cluster_oidc_issuer_url, "https://", "")
+
+  role_policy_arns = [
+    aws_iam_policy.external_dns_public.arn
+  ]
+  oidc_fully_qualified_subjects = [
+    "system:serviceaccount:external-dns:external-dns-public"
+  ]
+
+  tags = {
+    Subject = "external-dns-public"
+    Purpose = "eks-oidc"
+  }
+}
