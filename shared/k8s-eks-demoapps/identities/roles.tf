4da594826d2607ccfac56e9acfa6ed4d3a3fb674
Diego OJ <diego.ojeda@binbash.com.ar>
2021-05-13 10:28:56 -0300

    Enable auth on AWS ES

--- shared/k8s-eks-demoapps/identities/roles.tf
+++ shared/k8s-eks-demoapps/identities/roles.tf
@@ -43,3 +43,26 @@ module "role_external_dns_private" {
     Purpose = "eks-demoapps-oidc"
   }
 }
+
+#
+# Role: aws-es-proxy
+#
+module "role_aws_es_proxy" {
+  source = "github.com/binbashar/terraform-aws-iam.git//modules/iam-assumable-role-with-oidc?ref=v3.8.0"
+
+  create_role  = true
+  role_name    = "demoapps-aws-es-proxy"
+  provider_url = replace(data.terraform_remote_state.apps-devstg-eks-demoapps-cluster.outputs.cluster_oidc_issuer_url, "https://", "")
+
+  role_policy_arns = [
+    aws_iam_policy.demoapps_aws_es_proxy.arn
+  ]
+  oidc_fully_qualified_subjects = [
+    "system:serviceaccount:monitoring:aws-es-proxy"
+  ]
+
+  tags = {
+    Subject = "aws-es-proxy"
+    Purpose = "eks-demoapps-oidc"
+  }
+}
