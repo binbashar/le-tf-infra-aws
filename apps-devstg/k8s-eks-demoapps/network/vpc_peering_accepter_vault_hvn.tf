0bfc09be48ac33b001356d8e89dff9004c00ad43
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2021-05-25 13:44:03 -0300

    Create peering between EKS DemoApps and HCP (#240)

--- apps-devstg/k8s-eks-demoapps/network/vpc_peering_accepter_vault_hvn.tf
+++ apps-devstg/k8s-eks-demoapps/network/vpc_peering_accepter_vault_hvn.tf
@@ -1,14 +1,14 @@
 #
 # VPC Peering Connection with Apps Dev: Accepter Side
 #
-# resource "aws_vpc_peering_connection_accepter" "with_vault_hvn" {
-#   count = var.vpc_vault_hvn_created == true ? 1 : 0
+resource "aws_vpc_peering_connection_accepter" "with_vault_hvn" {
+  count = var.vpc_vault_hvn_created == true ? 1 : 0
 
-#   vpc_peering_connection_id = var.vpc_vault_hvn_peering_connection_id
-#   auto_accept               = true
+  vpc_peering_connection_id = var.vpc_vault_hvn_peering_connection_id
+  auto_accept               = true
 
-#   tags = merge(map("Name", "accepter-shared-from-vault-hvn"), local.tags)
-# }
+  tags = merge(map("Name", "accepter-eks-demoapps-from-vault-hvn"), local.tags)
+}
 
 #
 # Update Route Tables to go through the VPC Peering Connection
@@ -18,18 +18,18 @@
 # reducing public IPs exposure whenever possible.
 # read more: https://github.com/binbashar/le-tf-infra-aws/issues/49
 #
-# resource "aws_route" "priv_route_table_1_to_vault_hvn_vpc" {
-#   count = var.vpc_vault_hvn_created == true ? 1 : 0
+resource "aws_route" "priv_route_table_1_to_vault_hvn_vpc" {
+  count = var.vpc_vault_hvn_created == true ? 1 : 0
 
-#   route_table_id            = element(module.vpc-eks.private_route_table_ids, 0)
-#   destination_cidr_block    = var.vpc_vault_hvn_cird
-#   vpc_peering_connection_id = var.vpc_vault_hvn_peering_connection_id
-# }
+  route_table_id            = element(module.vpc-eks.private_route_table_ids, 0)
+  destination_cidr_block    = var.vpc_vault_hvn_cird
+  vpc_peering_connection_id = var.vpc_vault_hvn_peering_connection_id
+}
 
-# resource "aws_route" "pub_route_table_1_to_vault_hvn_vpc" {
-#   count = var.vpc_vault_hvn_created == true ? 1 : 0
+resource "aws_route" "pub_route_table_1_to_vault_hvn_vpc" {
+  count = var.vpc_vault_hvn_created == true ? 1 : 0
 
-#   route_table_id            = element(module.vpc-eks.public_route_table_ids, 0)
-#   destination_cidr_block    = var.vpc_vault_hvn_cird
-#   vpc_peering_connection_id = var.vpc_vault_hvn_peering_connection_id
-# }
+  route_table_id            = element(module.vpc-eks.public_route_table_ids, 0)
+  destination_cidr_block    = var.vpc_vault_hvn_cird
+  vpc_peering_connection_id = var.vpc_vault_hvn_peering_connection_id
+}
