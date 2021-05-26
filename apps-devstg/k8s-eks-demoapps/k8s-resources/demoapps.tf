b1612f8d59c01b9730573d4da3a01596a0d43957
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2021-05-26 10:50:51 -0300

    Add flags to enable DemoApps (#241)
    * Add flags to enable DemoApps
    
    * Fix trailing whitespace

--- apps-devstg/k8s-eks-demoapps/k8s-resources/demoapps.tf
+++ apps-devstg/k8s-eks-demoapps/k8s-resources/demoapps.tf
@@ -14,25 +14,31 @@
 #------------------------------------------------------------------------------
 # Google Microservices Demo
 #------------------------------------------------------------------------------
-# resource "helm_release" "gmd" {
-#   name       = "gmd"
-#   namespace  = kubernetes_namespace.argo_cd.id
-#   repository = "https://binbashar.github.io/helm-charts/"
-#   chart      = "argocd-application"
-#   version    = "0.2.0"
-#   values     = [ file("chart-values/demoapps-gmd.yaml") ]
-#   depends_on = [ helm_release.argo_cd ]
-# }
+resource "helm_release" "gmd" {
+  count      = var.enable_demoapps_gmd ? 1 : 0
+  name       = "gmd"
+  namespace  = kubernetes_namespace.argo_cd.id
+  repository = "https://binbashar.github.io/helm-charts/"
+  chart      = "argocd-application"
+  version    = "0.2.0"
+  values = [
+    templatefile("chart-values/demoapps-gmd.yaml", local.demoapps.gmd.templateValues)
+  ]
+  depends_on = [helm_release.argo_cd]
+}
 
 #------------------------------------------------------------------------------
 # Weave Sock-Shop Microservices Demo
 #------------------------------------------------------------------------------
-# resource "helm_release" "sockshop" {
-#   name       = "sockshop"
-#   namespace  = kubernetes_namespace.argo_cd.id
-#   repository = "https://binbashar.github.io/helm-charts/"
-#   chart      = "argocd-application"
-#   version    = "0.2.0"
-#   values     = [ file("chart-values/demoapps-sockshop.yaml") ]
-#   depends_on = [ helm_release.argo_cd ]
-# }
+resource "helm_release" "sockshop" {
+  count      = var.enable_demoapps_sockshop ? 1 : 0
+  name       = "sockshop"
+  namespace  = kubernetes_namespace.argo_cd.id
+  repository = "https://binbashar.github.io/helm-charts/"
+  chart      = "argocd-application"
+  version    = "0.2.0"
+  values = [
+    templatefile("chart-values/demoapps-sockshop.yaml", local.demoapps.sockshop.templateValues)
+  ]
+  depends_on = [helm_release.argo_cd]
+}
