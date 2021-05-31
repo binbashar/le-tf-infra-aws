a6dd091daf0baca6bae39e16c1fed3f3365acc44
Luis M. Gallardo D <lgallard@gmail.com>
2021-05-30 22:16:39 -0300

    * Ugrade app-prd/base-identities to TF 14 * Bump module versions for aws-iam (user, group, role) role

--- apps-prd/base-identities/outputs.tf
+++ apps-prd/base-identities/outputs.tf
@@ -5,17 +5,17 @@
 #
 output "user_auditor_ci_name" {
   description = "The user's name"
-  value       = module.user_auditor_ci.this_iam_user_name
+  value       = module.user_auditor_ci.iam_user_name
 }
 
 output "user_auditor_ci_iam_access_key_id" {
   description = "The aws aim access key"
-  value       = module.user_auditor_ci.this_iam_access_key_id
+  value       = module.user_auditor_ci.iam_access_key_id
   sensitive   = true
 }
 
 output "user_auditor_ci_iam_access_key_encrypted_secret" {
   description = "The encrypted secret key, base64 encoded"
-  value       = module.user_auditor_ci.this_iam_access_key_encrypted_secret
+  value       = module.user_auditor_ci.iam_access_key_encrypted_secret
   sensitive   = true
 }
