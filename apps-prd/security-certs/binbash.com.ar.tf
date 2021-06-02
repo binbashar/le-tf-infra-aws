3b509848cc68ff9fc37f52192d8225d35fd53295
Luis M. Gallardo D <lgallard@gmail.com>
2021-06-02 00:37:17 -0300

    Ugrade app-prd/security-cert to TF 0.14.11

--- apps-prd/security-certs/binbash.com.ar.tf
+++ apps-prd/security-certs/binbash.com.ar.tf
@@ -13,25 +13,36 @@ resource "aws_acm_certificate" "binbash_com_ar" {
   }
 }
 
+# AWS Provider > 0.31.1 changes
+# https://github.com/hashicorp/terraform/issues/26043
+# https://github.com/hashicorp/terraform-provider-aws/issues/10098#issuecomment-663562342
 resource "aws_acm_certificate_validation" "binbash_com_ar" {
-  certificate_arn = aws_acm_certificate.binbash_com_ar.arn
-  validation_record_fqdns = [
-    aws_route53_record.cert_validation_binbash_com_ar.fqdn
-  ]
+  certificate_arn         = aws_acm_certificate.binbash_com_ar.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation_binbash_com_ar : record.fqdn]
 }
 
 # Here we need a different AWS provider because CloudFront certificates
 # DNS validation records needs to be created in binbash-shared account
 #
 # binbash-shared route53 cross-account ACM dns validation update
+# AWS Provider > 0.31.1 changes
+# https://github.com/hashicorp/terraform/issues/26043
+# https://github.com/hashicorp/terraform-provider-aws/issues/10098#issuecomment-663562342
 resource "aws_route53_record" "cert_validation_binbash_com_ar" {
   provider = aws.shared-route53
-  name     = aws_acm_certificate.binbash_com_ar.domain_validation_options.0.resource_record_name
-  type     = aws_acm_certificate.binbash_com_ar.domain_validation_options.0.resource_record_type
-  zone_id  = data.terraform_remote_state.dns-shared.outputs.aws_public_zone_id[0]
 
-  records = [
-    aws_acm_certificate.binbash_com_ar.domain_validation_options.0.resource_record_value
-  ]
-  ttl = 60
+  for_each = {
+    for dvo in aws_acm_certificate.aws_binbash_com_ar.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 60
+  type    = each.value.type
+
+  zone_id = data.terraform_remote_state.dns-shared.outputs.aws_public_zone_id[0]
+
 }
