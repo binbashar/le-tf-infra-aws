03dd7d8e5a23ae35777887762aa23163ddc91e07
Luis M. Gallardo D <lgallard@gmail.com>
2021-06-09 01:08:50 -0300

    Upgrade shared/backups to TF 0.14.11

--- shared/backups/main.tf
+++ shared/backups/main.tf
@@ -2,7 +2,7 @@
 # Run daily backups on tagged resources
 #
 module "daily_backups_tagged_resources" {
-  source = "github.com/binbashar/terraform-aws-backup.git?ref=0.6.0"
+  source = "github.com/binbashar/terraform-aws-backup.git?ref=0.11.6"
 
   # Backup vaults are containers where your backups are stored. You can specify
   # a new one to be created by this module or omit it to use the default one.
@@ -36,38 +36,25 @@ module "daily_backups_tagged_resources" {
   selections = [
     {
       name = "tagged-resources"
-      selection_tag = {
-        type  = "STRINGEQUALS"
-        key   = "Backup"
-        value = "True"
-      }
+      selection_tags = [
+        {
+          type  = "STRINGEQUALS"
+          key   = "Backup"
+          value = "True"
+        },
+      ]
     },
   ]
 
-  tags = local.tags
-}
-
-module "daily_backups_notifications" {
-  source = "github.com/binbashar/terraform-aws-backup-notifications.git?ref=0.0.1"
-
-  enabled           = true
-  backup_vault_name = local.vault_name
-  backup_vault_events = [
-    "BACKUP_JOB_STARTED",
-    "BACKUP_JOB_FAILED",
-    "BACKUP_JOB_SUCCESSFUL",
-  ]
-
-  # You can either pass the ARN of an existing SNS Topic
-  sns_topic_arn = data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring_sec
-
-  # Or have the module create a new SNS Topic to which the following subscriptors will be attached
-  topic_subscriptions = {
-    # notify_slack = {
-    #   protocol               = "lambda"
-    #   endpoint               = data.terraform_remote_state.notifications.outputs.notify_slack_monitoring_sec_lambda_function_arn_monitoring_sec
-    #   endpoint_auto_confirms = true
-    #   raw_message_delivery   = true
-    # }
+  # Notifications
+  notifications = {
+    sns_topic_arn = data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring_sec
+    backup_vault_events = [
+      "BACKUP_JOB_STARTED",
+      "BACKUP_JOB_FAILED",
+      "BACKUP_JOB_SUCCESSFUL",
+    ]
   }
+
+  tags = local.tags
 }
