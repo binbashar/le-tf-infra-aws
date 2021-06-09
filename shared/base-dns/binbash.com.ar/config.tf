962ccd2c639d4ccc58b85ffe6d3fb68d4907adb0
Luis M. Gallardo D <lgallard@gmail.com>
2021-06-09 01:30:50 -0300

    * Upgrade shared/base-dns to TF 0.14.11 * Add eks-demoapps VPC to Route 53 private zone

--- shared/base-dns/binbash.com.ar/config.tf
+++ shared/base-dns/binbash.com.ar/config.tf
@@ -25,11 +25,7 @@ provider "aws" {
 # Backend Config (partial)    #
 #=============================#
 terraform {
-  required_version = ">= 0.14.4"
-
-  required_providers {
-    aws = "~> 3.2"
-  }
+  required_version = ">= 0.14.11"
 
   required_providers {
     aws = "~> 3.2"
@@ -77,6 +73,17 @@ data "terraform_remote_state" "dns-apps-devstg-kops" {
     profile = "${var.project}-apps-devstg-devops"
     bucket  = "${var.project}-apps-devstg-terraform-backend"
     key     = "apps-devstg/k8s-kops/prerequisites/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "vpc-apps-devstg-eks-demoapps" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = "${var.project}-apps-devstg-devops"
+    bucket  = "${var.project}-apps-devstg-terraform-backend"
+    key     = "apps-devstg/k8s-eks-demoapps/network/terraform.tfstate"
   }
 }
 
