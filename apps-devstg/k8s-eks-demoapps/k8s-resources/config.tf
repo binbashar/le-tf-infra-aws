4b2b844b60a3b793bd754b8183b304d69fdb3967
Luis M. Gallardo D <lgallard@gmail.com>
2021-06-16 00:19:53 -0300

    Upgrade apps-devstg/k8s-eks-demoapps TF 0.14.11

--- apps-devstg/k8s-eks-demoapps/k8s-resources/config.tf
+++ apps-devstg/k8s-eks-demoapps/k8s-resources/config.tf
@@ -25,7 +25,7 @@ provider "helm" {
 # Backend Config (partial)
 #
 terraform {
-  required_version = ">= 0.13.6"
+  required_version = ">= 0.14.11"
 
   required_providers {
     aws        = "~> 3.28"
