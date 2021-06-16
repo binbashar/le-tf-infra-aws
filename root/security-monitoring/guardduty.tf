2c98289c1b938cf00c37257e80b37da6c411fe18
Luis M. Gallardo D <lgallard@gmail.com>
2021-06-16 20:56:57 -0300

    Upgrade root/security-monitoring to TF 0.14.11

--- root/security-monitoring/guardduty.tf
+++ root/security-monitoring/guardduty.tf
@@ -5,7 +5,7 @@
 # Also designate Security account as GuardDuty's delegated admin
 #
 module "guardduty" {
-  source = "github.com/binbashar/terraform-aws-guardduty-multiaccount.git//modules/delegated-admin?ref=v0.0.3"
+  source = "github.com/binbashar/terraform-aws-guardduty-multiaccount.git//modules/delegated-admin?ref=v0.0.7"
 
   guarduty_enabled                     = true
   guardduty_delegated_admin_account_id = var.security_account_id
