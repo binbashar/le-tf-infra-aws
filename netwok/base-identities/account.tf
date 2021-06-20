7cfd072893f544be6fb427988357267a403f9a1c
Luis M. Gallardo D <lgallard@gmail.com>
2021-06-20 00:07:33 -0300

    Add network account definitions

---
+++ netwok/base-identities/account.tf
@@ -0,0 +1,25 @@
+#
+# Account Resources
+#
+module "aws_iam_account_config" {
+  source = "github.com/binbashar/terraform-aws-iam.git//modules/iam-account?ref=v4.1.0"
+
+  // If IAM account alias was previously set (either via AWS console or during the creation of an account from AWS
+  // Organizations) you will see this error:
+  // * aws_iam_account_alias.this: Error creating account alias with name my-account-alias
+  // please check https://github.com/binbashar/terraform-aws-iam/tree/master/modules/iam-account to solve it
+  account_alias = "${var.project_long}-${var.environment}"
+
+  # account password policy
+  create_account_password_policy = true
+  max_password_age               = 60
+  minimum_password_length        = 30
+  require_numbers                = true
+  require_lowercase_characters   = true
+  require_symbols                = true
+  require_uppercase_characters   = true
+  password_reuse_prevention      = 5
+  allow_users_to_change_password = true
+}
+
+data "aws_caller_identity" "current" {}
---
+++ netwok/base-identities/users.tf
@@ -0,0 +1,39 @@
+#
+# AWS IAM Users (alphabetically ordered)
+#
+# Machine / Automation Users
+#
+
+#==========================#
+# User: AuditorCI          #
+#==========================#
+module "user_auditor_ci" {
+  source = "github.com/binbashar/terraform-aws-iam.git//modules/iam-user?ref=v4.1.0"
+
+  name                    = "auditor.ci"
+  force_destroy           = true
+  password_reset_required = true
+
+  create_iam_user_login_profile = false
+  create_iam_access_key         = true
+  upload_iam_user_ssh_key       = false
+
+  pgp_key = file("keys/machine.auditor.ci")
+}
+
+#==========================#
+# User: BackupS3           #
+#==========================#
+module "user_backup_s3" {
+  source = "github.com/binbashar/terraform-aws-iam.git//modules/iam-user?ref=v4.1.0"
+
+  name                    = "backup.s3"
+  force_destroy           = true
+  password_reset_required = true
+
+  create_iam_user_login_profile = false
+  create_iam_access_key         = true
+  upload_iam_user_ssh_key       = false
+
+  pgp_key = file("keys/machine.backup.s3")
+}
