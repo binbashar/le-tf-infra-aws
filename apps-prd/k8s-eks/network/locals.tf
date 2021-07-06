fceb0b05da00a955a0965f62a56244709dc3fdfc
Luis M. Gallardo D <lgallard@gmail.com>
2021-07-06 15:08:34 -0300

    Add apps-prd/k8s-eks/network layer

---
+++ apps-prd/k8s-eks/network/locals.tf
@@ -0,0 +1,144 @@
+locals {
+
+  cluster_name = "${var.project}-${var.environment}-eks-prd"
+
+  # Network Local Vars
+  # https://docs.aws.amazon.com/eks/latest/userguide/network_reqs.html
+  # Important
+  # Docker runs in the 172.17.0.0/16 CIDR range in Amazon EKS clusters. We recommend that your cluster's VPC subnets do
+  # not overlap this range. Otherwise, you will receive the following error:
+  # Error: : error upgrading connection: error dialing backend: dial tcp 172.17.nn.nn:10250: getsockopt: no route to host
+  vpc_name       = "${var.project}-${var.environment}-vpc-eks"
+  vpc_cidr_block = "172.19.32.0/20"
+  azs = [
+    "${var.region}a",
+    "${var.region}b",
+    "${var.region}c",
+  ]
+
+  private_subnets = [
+    "172.19.32.0/23",
+    "172.19.34.0/23",
+    "172.19.36.0/23",
+  ]
+
+  public_subnets = [
+    "172.19.38.0/23",
+    "172.19.40.0/23",
+    "172.19.42.0/23",
+  ]
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.environment
+  }
+
+  # We need these so that k8s aws cloud provider recognizes our private subnets
+  # and associates them to any load balancer that is created
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+}
+
+locals {
+
+  # private inbounds
+  private_inbound = flatten([
+    for index, state in local.datasources-vpcs : [
+      {
+        rule_number = 10 * (index(keys(local.datasources-vpcs), index) + 1)
+        rule_action = "allow"
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "all"
+        cidr_block  = state.outputs.vpc_cidr_block
+      }
+    ]
+  ])
+
+  network_acls = {
+    #
+    # Allow / Deny VPC private subnets inbound default traffic
+    #
+    default_inbound = [
+      {
+        rule_number = 900 # shared pritunl vpn server
+        rule_action = "allow"
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "all"
+        cidr_block  = "${data.terraform_remote_state.tools-vpn-server.outputs.instance_private_ip}/32"
+      },
+      {
+        rule_number = 910 # vault hvn vpc
+        rule_action = "allow"
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "all"
+        cidr_block  = var.vpc_vault_hvn_cird
+      },
+      {
+        rule_number = 920 # NTP traffic
+        rule_action = "allow"
+        from_port   = 123
+        to_port     = 123
+        protocol    = "udp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 930 # Fltering known TCP ports (0-1024)
+        rule_action = "allow"
+        from_port   = 1024
+        to_port     = 65525
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 940 # Fltering known UDP ports (0-1024)
+        rule_action = "allow"
+        from_port   = 1024
+        to_port     = 65525
+        protocol    = "udp"
+        cidr_block  = "0.0.0.0/0"
+      },
+    ]
+
+    #
+    # Allow VPC private subnets inbound traffic
+    #
+    private_inbound = local.private_inbound
+  }
+
+  # Data source definitions
+  #
+
+  # shared
+  shared-vpcs = {
+    shared = {
+      region  = var.region
+      profile = "${var.project}-shared-devops"
+      bucket  = "${var.project}-shared-terraform-backend"
+      key     = "shared/network/terraform.tfstate"
+    }
+  }
+
+  # network
+  network-vpcs = {
+    base = {
+      region  = var.region
+      profile = "${var.project}-network-devops"
+      bucket  = "${var.project}-network-terraform-backend"
+      key     = "network/network/terraform.tfstate"
+    }
+  }
+
+  datasources-vpcs = merge(
+    data.terraform_remote_state.network-vpcs, # network
+    data.terraform_remote_state.shared-vpcs,  # shared
+  )
+}
---
+++ apps-prd/k8s-eks/network/network.auto.tfvars
@@ -0,0 +1,2 @@
+vpc_enable_nat_gateway   = false
+vpc_apps_prd_eks_created = false
