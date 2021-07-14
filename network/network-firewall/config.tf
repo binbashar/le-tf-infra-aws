30212674af033853c2fc76cc2f3d13ab18189ebb
Luis M. Gallardo D <lgallard@gmail.com>
2021-07-14 17:14:14 -0300

    Add network-firewall layer

---
+++ network/network-firewall/config.tf
@@ -0,0 +1,44 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  region                  = var.region
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/${var.project}/config"
+}
+
+#=============================#
+# Backend Config (partial)    #
+#=============================#
+terraform {
+  required_version = ">= 0.14.11"
+
+  required_providers {
+    aws = "~> 3.0"
+  }
+
+  backend "s3" {
+    key = "network/network-firewall/terraform.tfstate"
+  }
+}
+
+#=============================#
+# Data sources                #
+#=============================#
+
+
+# VPC remote states for network
+data "terraform_remote_state" "network-vpcs" {
+  for_each = local.network-vpcs
+
+  backend = "s3"
+
+  config = {
+    region  = lookup(each.value, "region")
+    profile = lookup(each.value, "profile")
+    bucket  = lookup(each.value, "bucket")
+    key     = lookup(each.value, "key")
+  }
+
+}
+
---
+++ network/network-firewall/variables.tf
@@ -0,0 +1,228 @@
+#
+# config/backend.config
+#
+#================================#
+# Terraform AWS Backend Settings #
+#================================#
+variable "region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "profile" {
+  type        = string
+  description = "AWS Profile (required by the backend but also used for other resources)"
+}
+
+variable "bucket" {
+  type        = string
+  description = "AWS S3 TF State Backend Bucket"
+}
+
+variable "dynamodb_table" {
+  type        = string
+  description = "AWS DynamoDB TF Lock state table name"
+}
+
+variable "encrypt" {
+  type        = bool
+  description = "Enable AWS DynamoDB with server side encryption"
+}
+
+#
+# config/base.config
+#
+#=============================#
+# Project Variables           #
+#=============================#
+variable "project" {
+  type        = string
+  description = "Project Name"
+}
+
+variable "project_long" {
+  type        = string
+  description = "Project Long Name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment Name"
+}
+
+#
+# config/extra.config
+#
+#=============================#
+# Accounts & Extra Vars       #
+#=============================#
+variable "region_secondary" {
+  type        = string
+  description = "AWS Scondary Region for HA"
+}
+
+variable "root_account_id" {
+  type        = string
+  description = "Account: Root"
+}
+
+variable "security_account_id" {
+  type        = string
+  description = "Account: Security & Users Management"
+}
+
+variable "shared_account_id" {
+  type        = string
+  description = "Account: Shared Resources"
+}
+
+variable "appsdevstg_account_id" {
+  type        = string
+  description = "Account: Dev Modules & Libs"
+}
+
+variable "appsprd_account_id" {
+  type        = string
+  description = "Account: Prod Modules & Libs"
+}
+
+variable "vault_address" {
+  type        = string
+  description = "Vault Address"
+}
+
+variable "vault_token" {
+  type        = string
+  description = "Vault Token"
+}
+
+#===========================================#
+# Networking                                #
+#===========================================#
+variable "vpc_apps_devstg_created" {
+  description = "true if Apps Dev account VPC is created for Peering purposes"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_apps_devstg_eks_created" {
+  description = "true if Apps Dev account EKS VPC is created for Peering purposes"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_apps_prd_created" {
+  description = "true if Apps Prd account VPC is created for Peering purposes"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_vault_hvn_created" {
+  description = "true if the Hahicorp Vault Cloud HVN account VPC is created for Peering purposes"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_vault_hvn_peering_connection_id" {
+  description = "Hahicorp Vault Cloud HVN VPC peering ID"
+  type        = string
+  default     = "pcx-0109e4ef7e784ee06"
+}
+
+variable "vpc_vault_hvn_cird" {
+  description = "Hahicorp Vault Cloud HVN VPC CIDR segment"
+  type        = string
+  default     = "172.25.16.0/20"
+}
+
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT Gatewway"
+  type        = bool
+  default     = false
+}
+
+variable "vpc_single_nat_gateway" {
+  description = "Single NAT Gatewway"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_enable_dns_hostnames" {
+  description = "Enable DNS HOSTNAME"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_enable_vpn_gateway" {
+  description = "Enable VPN Gateway"
+  type        = bool
+  default     = false
+}
+
+variable "vpc_enable_s3_endpoint" {
+  description = "Enable S3 endpoint"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_enable_dynamodb_endpoint" {
+  description = "Enable DynamoDB endpoint"
+  type        = bool
+  default     = true
+}
+
+variable "enable_kms_endpoint" {
+  description = "Enable KMS endpoint"
+  type        = bool
+  default     = false
+}
+
+variable "manage_default_network_acl" {
+  description = "Manage default Network ACL"
+  type        = bool
+  default     = false
+}
+
+variable "public_dedicated_network_acl" {
+  description = "Manage default Network ACL"
+  type        = bool
+  default     = true
+}
+
+variable "private_dedicated_network_acl" {
+  description = "Manage default Network ACL"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_endpoints" {
+  description = "VPC endpoints"
+  type        = any
+  default = {
+    s3 = {
+      service      = "s3"
+      service_type = "Gateway"
+    }
+    dynamodb = {
+      service      = "dynamodb"
+      service_type = "Gateway"
+    }
+  }
+}
+
+variable "enable_tgw" {
+  description = "Enable Transit Gateway Support"
+  type        = bool
+  default     = true
+}
+
+variable "enable_vpc_attach" {
+  description = "Enable VPC attachments per account"
+  type        = any
+  default = {
+    network     = false
+    shared      = false
+    apps-devstg = false
+    apps-prd    = false
+  }
+}
