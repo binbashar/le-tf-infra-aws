588facad94486676305da1f724e4a11c3ee82dcc
Luis M. Gallardo D <lgallard@gmail.com>
2021-07-13 22:41:27 -0300

    Fix NACL in apps-prd/base-network

--- apps-prd/base-network/network_vpc_peering_requester_apps_prd_to_shared.tf
+++ apps-prd/base-network/network_vpc_peering_requester_apps_prd_to_shared.tf
@@ -9,10 +9,10 @@
 # VPC Apps Prd w/ Shared
 #
 resource "aws_vpc_peering_connection" "apps_prd_vpc_with_shared_vpc" {
-  count = var.vpc_shared_created && !data.terraform_remote_state.vpc-network.outputs.enable_tgw ? 1 : 0
+  count = var.vpc_shared_created && !data.terraform_remote_state.network-vpcs["network-base"].outputs.enable_tgw ? 1 : 0
 
   peer_owner_id = var.shared_account_id
-  peer_vpc_id   = data.terraform_remote_state.vpc-shared.outputs.vpc_id
+  peer_vpc_id   = data.terraform_remote_state.shared-vpcs["shared-base"].outputs.vpc_id
   vpc_id        = module.vpc.vpc_id
   auto_accept   = false
 
@@ -28,17 +28,17 @@ resource "aws_vpc_peering_connection" "apps_prd_vpc_with_shared_vpc" {
 # read more: https://github.com/binbashar/le-tf-infra-aws/issues/49
 #
 resource "aws_route" "priv_route_table_1_apps_prd_vpc_to_shared_vpc" {
-  count = var.vpc_shared_created && !data.terraform_remote_state.vpc-network.outputs.enable_tgw ? 1 : 0
+  count = var.vpc_shared_created && !data.terraform_remote_state.network-vpcs["network-base"].outputs.enable_tgw ? 1 : 0
 
   route_table_id            = element(module.vpc.private_route_table_ids, 0)
-  destination_cidr_block    = data.terraform_remote_state.vpc-shared.outputs.vpc_cidr_block
+  destination_cidr_block    = data.terraform_remote_state.shared-vpcs["shared-base"].outputs.vpc_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.apps_prd_vpc_with_shared_vpc[0].id
 }
 
 resource "aws_route" "pub_route_table_1_apps_prd_vpc_to_shared_vpc" {
-  count = var.vpc_shared_created && !data.terraform_remote_state.vpc-network.outputs.enable_tgw ? 1 : 0
+  count = var.vpc_shared_created && !data.terraform_remote_state.network-vpcs["network-base"].outputs.enable_tgw ? 1 : 0
 
   route_table_id            = element(module.vpc.public_route_table_ids, 0)
-  destination_cidr_block    = data.terraform_remote_state.vpc-shared.outputs.vpc_cidr_block
+  destination_cidr_block    = data.terraform_remote_state.shared-vpcs["shared-base"].outputs.vpc_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.apps_prd_vpc_with_shared_vpc[0].id
 }
