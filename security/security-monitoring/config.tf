e76352bec9e4e02997035ec78a52c193593ef647
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2021-07-27 13:48:19 -0300

    #154 | security/security-monitoring-* layers integrated with vault hcp

--- security/security-monitoring/config.tf
+++ security/security-monitoring/config.tf
@@ -8,16 +8,34 @@ provider "aws" {
 }
 
 #=============================#
+# Vault Provider Settings     #
+#=============================#
+provider "vault" {
+  address = var.vault_address
+
+  /*
+  Vault token that will be used by Terraform to authenticate.
+ admin token from https://portal.cloud.hashicorp.com/.
+ */
+  token = var.vault_token
+}
+
+#=============================#
 # Backend Config (partial)    #
 #=============================#
 terraform {
   required_version = ">= 0.14.11"
 
   required_providers {
-    aws = "~> 3.2.0"
+    aws   = "~> 3.2"
+    vault = "~> 2.18.0"
   }
 
   backend "s3" {
     key = "security/security-monitoring/terraform.tfstate"
   }
+}
+
+data "vault_generic_secret" "slack_hook_url_monitoring" {
+  path = "secrets/${var.project}/${var.environment}/security-monitoring"
 }
