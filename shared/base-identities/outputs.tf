148f22ef301a87dba857fd66b142df9571b8ce1b
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2021-07-27 13:38:19 -0300

    #154 | shared/base-identities removing not necessary user

--- shared/base-identities/outputs.tf
+++ shared/base-identities/outputs.tf
@@ -19,20 +19,3 @@ output "user_auditor_ci_iam_access_key_encrypted_secret" {
   value       = module.user_auditor_ci.iam_access_key_encrypted_secret
   sensitive   = true
 }
-
-output "user_backup_se_name" {
-  description = "The user's name"
-  value       = module.user_backup_s3.iam_user_name
-}
-
-output "user_backup_s3_iam_access_key_id" {
-  description = "The aws aim access key"
-  value       = module.user_backup_s3.iam_access_key_id
-  sensitive   = true
-}
-
-output "user_backup_s3_iam_access_key_encrypted_secret" {
-  description = "The encrypted secret key, base64 encoded"
-  value       = module.user_backup_s3.iam_access_key_encrypted_secret
-  sensitive   = true
-}
