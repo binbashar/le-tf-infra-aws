9ecbaf13f2079335182bb55c3aec51e35a7bd113
Luis M. Gallardo D <lgallard@gmail.com>
2021-08-06 12:15:31 -0300

    Fix wrong inspection route table id output value

--- network/transit-gateway/outputs.tf
+++ network/transit-gateway/outputs.tf
@@ -10,7 +10,7 @@ output "tgw_route_table_id" {
 
 output "tgw_inspection_route_table_id" {
   description = "TGW inspection route table id"
-  value       = var.enable_tgw && var.enable_network_firewall && lookup(var.enable_vpc_attach, "network", false) ? module.tgw_vpc_attachments_and_subnet_routes_network_firewall["network-firewall"].transit_gateway_route_table_id : null
+  value       = var.enable_tgw && var.enable_network_firewall && lookup(var.enable_vpc_attach, "network", false) ? module.tgw_inspection_route_table[0].transit_gateway_route_table_id : null
 }
 
 output "enable_tgw" {
@@ -22,4 +22,3 @@ output "enable_vpc_attach" {
   description = "VPC attachments per account"
   value       = var.enable_vpc_attach
 }
-
