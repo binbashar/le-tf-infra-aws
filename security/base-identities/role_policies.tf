03914a99d5175b09061313c79ca429d6c138adf4
Luis M. Gallardo D <lgallard@gmail.com>
2021-09-14 00:18:16 -0300

    Add SecOps role

--- security/base-identities/role_policies.tf
+++ security/base-identities/role_policies.tf
@@ -110,3 +110,113 @@ resource "aws_iam_policy" "devops_access" {
 }
 EOF
 }
+
+#
+# User Managed Policy: SecOps Access
+#
+resource "aws_iam_policy" "secops_access" {
+  name        = "devops_access"
+  description = "Services enabled for SecOps role"
+
+  #
+  # IMPORTANT: Multiple condition keys in the same statement are not supported for some ec2 and rds actions.
+  #
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "MultiServiceFullAccessCustom",
+            "Effect": "Allow",
+            "Action": [
+                "access-analyzer:*",
+                "acm:*",
+                "aws-portal:*",
+                "backup:*",
+                "backup-storage:*",
+                "ce:*",
+                "cloudformation:*",
+                "cloudtrail:*",
+                "cloudwatch:*",
+                "config:*",
+                "dlm:*",
+                "dynamodb:*",
+                "ec2:*",
+                "events:*",
+                "fms:*",
+                "guardduty:*",
+                "health:*",
+                "iam:*",
+                "kms:*",
+                "lambda:*",
+                "logs:*",
+                "organizations:Describe*",
+                "organizations:List*",
+                "route53:*",
+                "route53domains:*",
+                "route53resolver:*",
+                "s3:*",
+                "sns:*",
+                "ssm:*",
+                "support:*",
+                "tag:*",
+                "trustedadvisor:*",
+                "vpc:*"
+            ],
+            "Resource": [
+                "*"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "aws:RequestedRegion": [
+                        "us-east-1",
+                        "us-east-2",
+                        "us-west-2"
+                    ]
+                }
+            }
+        },
+        {
+            "Sid": "Ec2RunInstanceCustomSize",
+            "Effect": "Deny",
+            "Action": "ec2:RunInstances",
+            "Resource": [
+                "arn:aws:ec2:*:*:instance/*"
+            ],
+            "Condition": {
+                "ForAnyValue:StringNotLike": {
+                    "ec2:InstanceType": [
+                        "*.nano",
+                        "*.micro",
+                        "*.small",
+                        "*.medium",
+                        "*.large"
+                    ]
+                }
+            }
+        },
+        {
+            "Sid": "RdsFullAccessCustomSize",
+            "Effect": "Deny",
+            "Action": [
+                "rds:CreateDBInstance",
+                "rds:CreateDBCluster"
+            ],
+            "Resource": [
+                "arn:aws:rds:*:*:db:*"
+            ],
+            "Condition": {
+                "ForAnyValue:StringNotLike": {
+                    "rds:DatabaseClass": [
+                        "*.micro",
+                        "*.small",
+                        "*.medium",
+                        "*.large"
+                    ]
+                }
+            }
+        }
+    ]
+}
+EOF
+}
