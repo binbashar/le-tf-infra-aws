d40abc3a578325bc88efb9bc42954596520f4ea2
Luis M. Gallardo D <lgallard@gmail.com>
2021-10-01 02:30:56 -0300

    Remove AWS config authorization resources

--- network/security-compliance/awsconfig_authorization.tf
+++
@@ -1,5 +0,0 @@
-# Manages AWS Config Aggregate Authorization
-resource "aws_config_aggregate_authorization" "config_aggregate_auth" {
-  account_id = var.security_account_id
-  region     = var.region
-}
