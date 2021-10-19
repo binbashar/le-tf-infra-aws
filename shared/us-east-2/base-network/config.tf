f045b241663f9faf17b4118a6928083246442114
Diego OJ <diego.ojeda@binbash.com.ar>
2021-10-19 10:29:39 -0300

    Create SockShop DemoApp ECR repositories in both regions; also create DR VPC in Shared

---
+++ shared/us-east-2/base-network/config.tf
@@ -0,0 +1,77 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  region                  = var.region_secondary
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/${var.project}/config"
+}
+
+#=============================#
+# Backend Config (partial)    #
+#=============================#
+terraform {
+  required_version = ">= 0.14.11"
+
+  required_providers {
+    aws = "~> 3.0"
+  }
+
+  backend "s3" {
+    key = "shared/network-dr/terraform.tfstate"
+  }
+}
+
+#=============================#
+# Data sources                #
+#=============================#
+
+#
+# data type from output for tools-ec2
+#
+data "terraform_remote_state" "tools-vpn-server" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/vpn/terraform.tfstate"
+  }
+}
+
+# VPC remote states for apps-devstg
+data "terraform_remote_state" "apps-devstg-vpcs" {
+
+  for_each = {
+    for k, v in local.apps-devstg-vpcs :
+    k => v if !v["tgw"]
+  }
+
+  backend = "s3"
+
+  config = {
+    region  = lookup(each.value, "region")
+    profile = lookup(each.value, "profile")
+    bucket  = lookup(each.value, "bucket")
+    key     = lookup(each.value, "key")
+  }
+}
+
+# VPC remote states for apps-prd
+data "terraform_remote_state" "apps-prd-vpcs" {
+
+  for_each = {
+    for k, v in local.apps-prd-vpcs :
+    k => v if !v["tgw"]
+  }
+
+  backend = "s3"
+
+  config = {
+    region  = lookup(each.value, "region")
+    profile = lookup(each.value, "profile")
+    bucket  = lookup(each.value, "bucket")
+    key     = lookup(each.value, "key")
+  }
+}
---
+++ shared/us-east-2/container-registry/ecr_repositories.tf
@@ -0,0 +1,31 @@
+#
+# Create all repositories from the list of repositories
+#
+module "ecr_repositories" {
+  for_each = local.repository_list
+
+  source = "github.com/binbashar/terraform-aws-ecr-cross-account.git?ref=1.0.2"
+
+  #
+  # Repository name
+  #
+  create         = each.value.create
+  namespace      = ""
+  name           = each.value.name
+  use_namespaces = false
+
+  #
+  # Permissions: define read or write access
+  #
+  allowed_read_principals  = each.value.read_permissions
+  allowed_write_principals = each.value.write_permissions
+
+  #
+  # Images Retention: set lifecycle policies
+  #
+  lifecycle_policy_rules       = local.default_lifecycle_policy_rules
+  lifecycle_policy_rules_count = length(local.default_lifecycle_policy_rules)
+
+  # Security: whether to scan images upon pushing to repository
+  scan_on_pushing = true
+}
