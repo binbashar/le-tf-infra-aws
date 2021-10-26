1e15272a3e82b2262ccafccbd57d146f4f88dc4d
Diego OJ <diego.ojeda@binbash.com.ar>
2021-10-26 14:07:02 -0300

    Create new peerings in a simpler way

--- shared/us-east-2/base-network/vpc_peerings.tf
+++ shared/us-east-2/base-network/vpc_peerings.tf
@@ -1,29 +1,27 @@
 #
-# VPC Peerings: keep in mind that both sides (requester and accepter) are
-# managed from this layer.
-# Note: requester is referred to as "this" whereas accepter is the "peer".
+# VPC Peering: Shared DR VPC => AppsDevStg EKS DR VPC
 #
-module "vpc_peerings_shared_to_devstg" {
+module "vpc_peering_shared_dr_to_devstg_eks_dr" {
   source = "github.com/binbashar/terraform-aws-vpc-peering.git?ref=v4.0.1"
 
-  # Pass the AWS providers so that both sides of the peering can be managed
+  # Pass both providers so that both sides (requester and accepter) can be managed
   providers = {
     aws.this = aws
     aws.peer = aws.devstg_eks_dr
   }
 
-  # Loop through all defined peerings
-  for_each = { for k, v in local.vpc_peerings_devstg : k => v }
+  # Requester is referred to as "this", whereas accepter is the "peer"
+  this_vpc_id = module.vpc.vpc_id
+  peer_vpc_id = data.terraform_remote_state.apps-devstg-vpcs["apps-devstg-k8s-eks-dr"].outputs.vpc_id
 
-  # Provide the VPCs that will be peered
-  this_vpc_id = each.value.this_vpc_id
-  peer_vpc_id = each.value.peer_vpc_id
+  # Specify which route tables should be updated
+  this_rts_ids = concat(module.vpc.private_route_table_ids, module.vpc.public_route_table_ids)
+  peer_rts_ids = concat(
+    data.terraform_remote_state.apps-devstg-vpcs["apps-devstg-k8s-eks-dr"].outputs.public_route_table_ids,
+    data.terraform_remote_state.apps-devstg-vpcs["apps-devstg-k8s-eks-dr"].outputs.private_route_table_ids
+  )
 
-  # Provide the Route Tables that will be updated to route traffic through the peering connection
-  this_rts_ids = each.value.this_route_table_ids
-  peer_rts_ids = each.value.peer_route_table_ids
-
-  # Automatically accept the peering request from the accepter side
+  # Automatically accept the peering request on the accepter side
   auto_accept_peering = true
 
   tags = merge(local.tags, {
@@ -33,27 +31,26 @@ module "vpc_peerings_shared_to_devstg" {
   })
 }
 
-module "vpc_peerings_shared_to_shared" {
+#
+# VPC Peering: Shared DR VPC => Shared VPC
+#
+module "vpc_peering_shared_dr_to_shared" {
   source = "github.com/binbashar/terraform-aws-vpc-peering.git?ref=v4.0.1"
 
-  # Pass the AWS providers so that both sides of the peering can be managed
   providers = {
     aws.this = aws
     aws.peer = aws.shared_main
   }
 
-  # Loop through all defined peerings
-  for_each = { for k, v in local.vpc_peerings_shared : k => v }
+  this_vpc_id = module.vpc.vpc_id
+  peer_vpc_id = data.terraform_remote_state.shared-vpcs["shared-vpc"].outputs.vpc_id
 
-  # Provide the VPCs that will be peered
-  this_vpc_id = each.value.this_vpc_id
-  peer_vpc_id = each.value.peer_vpc_id
+  this_rts_ids = concat(module.vpc.private_route_table_ids, module.vpc.public_route_table_ids)
+  peer_rts_ids = concat(
+    data.terraform_remote_state.shared-vpcs["shared-vpc"].outputs.public_route_table_ids,
+    data.terraform_remote_state.shared-vpcs["shared-vpc"].outputs.private_route_table_ids
+  )
 
-  # Provide the Route Tables that will be updated to route traffic through the peering connection
-  this_rts_ids = each.value.this_route_table_ids
-  peer_rts_ids = each.value.peer_route_table_ids
-
-  # Automatically accept the peering request from the accepter side
   auto_accept_peering = true
 
   tags = merge(local.tags, {
