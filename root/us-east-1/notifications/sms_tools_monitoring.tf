c4ddf1d94710ff3f23956c9a13f35e2b87e0cda5
Luis M. Gallardo D <lgallard@gmail.com>
2021-10-26 15:50:39 -0300

    Fix SNS policy & raw_message_delivery default option

--- root/us-east-1/notifications/sms_tools_monitoring.tf
+++ root/us-east-1/notifications/sms_tools_monitoring.tf
@@ -8,21 +8,37 @@ module "notify_sms" {
       protocol               = "sms"
       endpoint               = data.vault_generic_secret.notifications.data["phone1"]
       endpoint_auto_confirms = true
-      raw_message_delivery   = true
+      raw_message_delivery   = false
     }
     #phone2 = {
     #  protocol               = "sms"
     #  endpoint               = data.vault_generic_secret.notifications.data["phone2"]
     #  endpoint_auto_confirms = true
-    #  raw_message_delivery   = true
+    #  raw_message_delivery   = false
     #}
     #phone3 = {
     #  protocol               = "sms"
     #  endpoint               = data.vault_generic_secret.notifications.data["phone3"]
     #  endpoint_auto_confirms = true
-    #  raw_message_delivery   = true
+    #  raw_message_delivery   = false
     #}
   }
 
-  sqs_dlq_enabled = false
+  # Policy
+  sns_topic_policy_json = join("", data.aws_iam_policy_document.aws_sns_topic_policy.*.json)
+
+}
+
+data "aws_iam_policy_document" "aws_sns_topic_policy" {
+
+  policy_id = "SNSTopicsPub"
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    effect    = "Allow"
+    actions   = ["sns:Publish"]
+    resources = ["arn:aws:sns:${var.region}:${var.root_account_id}:${var.sns_topic_name_sms}"]
+  }
 }
