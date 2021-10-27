4d8d9cb6f210aea91c28440a0b1f72d90aa25861
Luis M. Gallardo D <lgallard@gmail.com>
2021-10-27 12:31:46 -0300

    Add Bugets for SMS notifications

--- root/us-east-1/cost-mgmt/main.tf
+++ root/us-east-1/cost-mgmt/main.tf
@@ -8,7 +8,7 @@ module "aws_cost_mgmt_billing_alert_50" {
   aws_env                   = "${var.project}-${var.environment}-50"
   monthly_billing_threshold = var.monthly_billing_threshold_50
   currency                  = var.currency
-  aws_sns_topic_arns        = [data.terraform_remote_state.notifications.outputs.sns_topic_arn_sms, data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring]
+  sns_topic_arns            = [data.terraform_remote_state.notifications.outputs.sns_topic_arn_sms, data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring]
 
   tags = local.tags
 }
@@ -20,7 +20,7 @@ module "aws_cost_mgmt_billing_alert_100" {
   aws_env                   = "${var.project}-${var.environment}-100"
   monthly_billing_threshold = var.monthly_billing_threshold_100
   currency                  = var.currency
-  aws_sns_topic_arns        = [data.terraform_remote_state.notifications.outputs.sns_topic_arn_sms, data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring]
+  sns_topic_arns            = [data.terraform_remote_state.notifications.outputs.sns_topic_arn_sms, data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring]
 
   tags = local.tags
 }
@@ -30,7 +30,8 @@ module "aws_cost_mgmt_billing_alert_100" {
 #
 # Budget = U$S100 at 75%
 module "aws_cost_mgmt_budget_notif_75" {
-  source = "github.com/binbashar/terraform-aws-cost-billing-alarm.git?ref=v1.0.14"
+  #source = "github.com/binbashar/terraform-aws-cost-budget.git?ref=v1.0.10"
+  source = "git::https://github.com/binbashar/terraform-aws-cost-budget.git?ref=feature/single-block"
 
   aws_env                = "${var.environment}-75-percent"
   currency               = var.currency
@@ -38,13 +39,15 @@ module "aws_cost_mgmt_budget_notif_75" {
   time_unit              = var.time_unit
   time_period_start      = var.time_period_start
   notification_threshold = var.notification_threshold_75
-  aws_sns_topic_arns     = [data.terraform_remote_state.notifications.outputs.sns_topic_arn_sms, data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring]
   aws_sns_account_id     = var.root_account_id
+  create_sns_topic       = false
+  sns_topic_arns         = [data.terraform_remote_state.notifications.outputs.sns_topic_arn_sms, data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring]
 }
 
 # Budget = U$S100 at 100%
 module "aws_cost_mgmt_budget_notif_100" {
-  source = "github.com/binbashar/terraform-aws-cost-billing-alarm.git?ref=v1.0.14"
+  #source = "github.com/binbashar/terraform-aws-cost-budget.git?ref=v1.0.10"
+  source = "git::https://github.com/binbashar/terraform-aws-cost-budget.git?ref=feature/single-block"
 
   aws_env                = "${var.environment}-100-percent"
   currency               = var.currency
@@ -52,6 +55,7 @@ module "aws_cost_mgmt_budget_notif_100" {
   time_unit              = var.time_unit
   time_period_start      = var.time_period_start
   notification_threshold = var.notification_threshold_100
-  aws_sns_topic_arns     = [data.terraform_remote_state.notifications.outputs.sns_topic_arn_sms, data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring]
   aws_sns_account_id     = var.root_account_id
+  create_sns_topic       = false
+  sns_topic_arns         = [data.terraform_remote_state.notifications.outputs.sns_topic_arn_sms, data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring]
 }
