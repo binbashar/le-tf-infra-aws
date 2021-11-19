0994bf34bca960cbc63bae069e78a43650f5eddf
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2021-11-19 11:49:42 -0300

    Disaster Recovery (DR) Management and Security Guardduty security-monitoring layers updated to latest terraform module versions

--- security/us-east-2/security-monitoring --/guardduty.tf
+++ security/us-east-2/security-monitoring --/guardduty.tf
@@ -2,15 +2,29 @@
 # GuardDuty is enabled in this account as a delegated admin
 #
 module "guardduty" {
-  source = "github.com/binbashar/terraform-aws-guardduty-multiaccount.git//modules/multiaccount-setup?ref=v0.0.9"
+  source = "github.com/binbashar/terraform-aws-guardduty-multiaccount.git//modules/multiaccount-setup?ref=v0.1.0"
 
-  guarduty_enabled                           = true
-  guarduty_s3_protection_enabled             = true
-  guardduty_organization_members_auto_enable = false
+  # Activating Guardduty & S3 protection in this account (security-account).
+  guarduty_enabled                                          = true
+  guarduty_s3_protection_enabled                            = true
+
+  # New Org Accounts will have Guardduty & S3 Protection automatically enabled
+  guardduty_organization_members_auto_enable                = true
+  guardduty_organization_members_s3_protection_auto_enable  = true
+
+  # Pre-existing Org Accounts (already members) have to be declared below
   guardduty_member_accounts = {
+    root = {
+      account_id = var.root_account_id
+      email      = "info@binbash.com.ar"
+    },
     shared = {
       account_id = var.shared_account_id
       email      = "binbash-aws-sr@binbash.com.ar"
+    },
+    network = {
+      account_id = var.network_account_id
+      email      = "binbash-aws-net@binbash.com.ar"
     },
     appsdevstg = {
       account_id = var.appsdevstg_account_id
@@ -19,10 +33,6 @@ module "guardduty" {
     appsprd = {
       account_id = var.appsprd_account_id
       email      = "info+binbash-aws-prd@binbash.com.ar"
-    },
-    root = {
-      account_id = var.root_account_id
-      email      = "info@binbash.com.ar"
     }
   }
 }
