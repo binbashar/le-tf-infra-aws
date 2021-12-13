f897ff26a7a6070d4dca8983c4c735e171b41703
Diego OJ <diego.ojeda@binbash.com.ar>
2021-12-13 13:29:42 -0300

    Update readme and outputs

--- apps-devstg/us-east-1/storage/file_transfer_sftp/outputs.tf
+++ apps-devstg/us-east-1/storage/file_transfer_sftp/outputs.tf
@@ -1,7 +1,17 @@
-# output "customers_buckets" {
-#   description = "Customers' buckets"
+output "customers_usernames" {
+  description = "Customers' usernames"
 
-#   value = {
-#     for k, mod in module.customers_buckets : k => mod.s3_bucket_id
-#   }
-# }
+  value = {
+    for k, mod in var.customers : k => mod.username
+  }
+}
+
+output "server_custom_endpoint" {
+  description = "Server Custome Endpoint"
+  value       = aws_route53_record.main.name
+}
+
+output "server_endpoint" {
+  description = "Server Endpoint"
+  value       = module.customer_sftp.sftp_server_endpoint
+}
