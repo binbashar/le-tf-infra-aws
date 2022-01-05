01d5716ebb3073002b8b84fa9d240676171315d8
Luis M. Gallardo D <lgallard@gmail.com>
2022-01-05 00:39:20 -0300

    Add route to the shared account in private RT for apps-devstg/k8s-eks

--- network/us-east-1/base-network/config.tf
+++ network/us-east-1/base-network/config.tf
@@ -39,7 +39,7 @@ provider "aws" {
 # Backend Config (partial)    #
 #=============================#
 terraform {
-  required_version = ">= 0.14.11"
+  required_version = ">= 1.0.9"
 
   required_providers {
     aws = "~> 3.0"
