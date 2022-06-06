df56c32490f7d4547cae0e92d52c73b7424eb059
Matias-R <matias.rodriguez@binbash.com.ar>
2022-06-06 17:30:49 -0300

    feat(apps-prd/backups): * Update providers versions tf:"~>1.1.3" and tf-aws:"~>4.0" * Remove "shared_credentials" parameter. * Add "tf.lock.hcl" file

--- apps-prd/us-east-1/backups --/config.tf
+++ apps-prd/us-east-1/backups --/config.tf
@@ -2,19 +2,19 @@
 # AWS Provider Settings       #
 #=============================#
 provider "aws" {
-  region                  = var.region
-  profile                 = var.profile
-  shared_credentials_file = "~/.aws/${var.project}/config"
+  region  = var.region
+  profile = var.profile
+  # shared_credentials_file = "~/.aws/${var.project}/config"
 }
 
 #=============================#
 # Backend Config (partial)    #
 #=============================#
 terraform {
-  required_version = ">= 0.14.11"
+  required_version = "~> 1.1.3"
 
   required_providers {
-    aws = "~> 3.2"
+    aws = "~> 4.0"
   }
 
   backend "s3" {
