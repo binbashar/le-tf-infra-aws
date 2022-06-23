2befeeb4dd99cb7cf846b028f81e4ee94a9448e8
Luis M. Gallardo D <lgallard@gmail.com>
2022-06-23 12:37:08 -0300

    Fix variables for shared/us-east-1/tools-vpn-server layer

--- shared/us-east-1/tools-vpn-server/variables.tf
+++ shared/us-east-1/tools-vpn-server/variables.tf
@@ -1,3 +1,66 @@
-#================================#
-# Local variables                #
-#================================#
+#=============================#
+# EC2 BASIC LAYOUT MODULE     #
+#=============================#
+#
+# General
+#
+variable "prefix" {
+  type        = string
+  description = "Prefix"
+  default     = "infra"
+}
+
+variable "name" {
+  type        = string
+  description = "Name"
+  default     = "vpn-pritunl"
+}
+
+#
+# EC2 Attributes
+#
+variable "aws_ami_os_id" {
+  description = "AWS AMI Operating System Identificator"
+  default     = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
+}
+
+variable "aws_ami_os_owner" {
+  description = "AWS AMI Operating System Owner, eg: 099720109477 for Canonical "
+  default     = "099720109477"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 Instance Type"
+  default     = "t3.micro"
+}
+
+variable "ebs_optimized" {
+  type        = string
+  description = "Enable EBS Optimized"
+  default     = "false"
+}
+
+variable "associate_public_ip_address" {
+  type        = bool
+  description = "Associate a public IP address with the instance"
+  default     = true
+}
+
+variable "root_device_backup_tag" {
+  type        = string
+  description = "EC2 Root Block Device backup tag"
+  default     = "True"
+}
+
+variable "monitoring" {
+  type        = bool
+  description = "If true, the launched EC2 instance will have detailed monitoring enabled"
+  default     = false
+}
+
+variable "tag_approved_ami_value" {
+  type        = string
+  description = "Set the specific tag ApprovedAMI ('true' | 'false') that identifies aws-config compliant AMIs"
+  default     = "true"
+}
