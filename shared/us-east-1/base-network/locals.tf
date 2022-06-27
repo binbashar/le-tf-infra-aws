51384452a1cdeac47423615b08651074fdc1950d
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2022-06-27 16:33:17 -0300

    updating peering with k8s-eks-v1.17 vpc

--- shared/us-east-1/base-network/locals.tf
+++ shared/us-east-1/base-network/locals.tf
@@ -129,11 +129,11 @@ locals {
       bucket  = "${var.project}-apps-devstg-terraform-backend"
       key     = "apps-devstg/network/terraform.tfstate"
     }
-    apps-devstg-k8s-eks = {
+    apps-devstg-k8s-eks-v117 = {
       region  = var.region
       profile = "${var.project}-apps-devstg-devops"
       bucket  = "${var.project}-apps-devstg-terraform-backend"
-      key     = "apps-devstg/k8s-eks/network/terraform.tfstate"
+      key     = "apps-devstg/k8s-eks-v1.17/network/terraform.tfstate"
     }
     apps-devstg-eks-demoapps = {
       region  = var.region
@@ -160,12 +160,6 @@ locals {
       profile = "${var.project}-apps-prd-devops"
       bucket  = "${var.project}-apps-prd-terraform-backend"
       key     = "apps-prd/network/terraform.tfstate"
-    }
-    apps-prd-k8s-eks = {
-      region  = var.region
-      profile = "${var.project}-apps-prd-devops"
-      bucket  = "${var.project}-apps-prd-terraform-backend"
-      key     = "apps-prd/k8s-eks/network/terraform.tfstate"
     }
   }
 
