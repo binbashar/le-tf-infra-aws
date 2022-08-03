35361cebd35d2cdfcfa5ece3c9af6d097515a74b
Ezequiel Godoy <contacto@ezegodoy.com>
2022-08-03 18:26:43 -0300

    Apply suggestions from code review
    Co-authored-by: Angelo Fenoglio <angelofenoglio@gmail.com>

--- network/global/base-identities/roles.tf
+++ network/global/base-identities/roles.tf
@@ -194,7 +194,7 @@ module "iam_assumable_role_oaar" {
   source = "github.com/binbashar/terraform-aws-iam.git//modules/iam-assumable-role?ref=v4.1.0"
 
   trusted_role_arns = [
-    "arn:aws:iam::${var.accounts.management.id}:root"
+    "arn:aws:iam::${var.accounts.root.id}:root"
   ]
 
   create_role           = true
