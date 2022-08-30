10d68a452c120bc62a33094e21a7bed3187b7ad8
marianod92 <mariano.dimodugno@binbash.com.ar>
2022-08-30 18:50:01 -0300

    Update Rules naming and sort priority by first blocked criteria

--- apps-devstg/us-east-1/security-firewall --/wafv2-regional.tf
+++ apps-devstg/us-east-1/security-firewall --/wafv2-regional.tf
@@ -21,9 +21,78 @@ module "wafv2_regional_alb" {
   }
 
   rules = [
+    ###Custom IP Rate Based Rule
+    {
+      name     = "CustomRulesIpRateLimitBasedRuleSet"
+      priority = "0"
+
+      action = "block"
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = true
+        metric_name                = "CustomRuleIpRateLimitBasedRuleSet-Metrics"
+        sampled_requests_enabled   = true
+      }
+
+      rate_based_statement = {
+        limit              = 2000
+        aggregate_key_type = "IP"
+      }
+    },
+    {
+      name     = "AWSManagedRulesAmazonIpReputationList"
+      priority = "1"
+
+      override_action = "none"
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = true
+        metric_name                = "AWSManagedRulesAmazonIpReputationList-Metrics"
+        sampled_requests_enabled   = true
+      }
+
+      managed_rule_group_statement = {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    },
+    {
+      name     = "AWSManagedRulesAnonymousIpList"
+      priority = "2"
+
+      override_action = "none"
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = true
+        metric_name                = "AWSManagedRulesAnonymousIpList-Metrics"
+        sampled_requests_enabled   = true
+      }
+
+      managed_rule_group_statement = {
+        name        = "AWSManagedRulesAnonymousIpList"
+        vendor_name = "AWS"
+      }
+    },
+    {
+      name     = "AWSManagedRulesBotControlRuleSet"
+      priority = "3"
+
+      override_action = "none"
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = true
+        metric_name                = "AWSManagedRulesBotControlRuleSet-Metrics"
+        sampled_requests_enabled   = true
+      }
+
+      managed_rule_group_statement = {
+        name        = "AWSManagedRulesBotControlRuleSet"
+        vendor_name = "AWS"
+      }
+    },
     {
       name     = "AWSManagedRulesCommonRuleSet"
-      priority = "1"
+      priority = "4"
 
       override_action = "none"
 
@@ -43,7 +112,7 @@ module "wafv2_regional_alb" {
     },
     {
       name     = "AWSManagedRulesKnownBadInputsRuleSet"
-      priority = "2"
+      priority = "5"
 
       override_action = "none"
 
@@ -60,7 +129,7 @@ module "wafv2_regional_alb" {
     },
     {
       name     = "AWSManagedRulesSQLiRuleSet"
-      priority = "3"
+      priority = "6"
 
       override_action = "none"
 
@@ -74,109 +143,6 @@ module "wafv2_regional_alb" {
         name        = "AWSManagedRulesSQLiRuleSet"
         vendor_name = "AWS"
       }
-    },
-    {
-      name     = "AWSManagedRulesAmazonIpReputationList"
-      priority = "4"
-
-      override_action = "none"
-
-      visibility_config = {
-        cloudwatch_metrics_enabled = true
-        metric_name                = "AWSManagedRulesAmazonIpReputationList-Metrics"
-        sampled_requests_enabled   = true
-      }
-
-      managed_rule_group_statement = {
-        name        = "AWSManagedRulesAmazonIpReputationList"
-        vendor_name = "AWS"
-      }
-    },
-    {
-      name     = "AWSManagedRulesAnonymousIpList"
-      priority = "5"
-
-      override_action = "none"
-
-      visibility_config = {
-        cloudwatch_metrics_enabled = true
-        metric_name                = "AWSManagedRulesAnonymousIpList-Metrics"
-        sampled_requests_enabled   = true
-      }
-
-      managed_rule_group_statement = {
-        name        = "AWSManagedRulesAnonymousIpList"
-        vendor_name = "AWS"
-      }
-    },
-    {
-      name     = "AWSManagedRulesBotControlRuleSet"
-      priority = "6"
-
-      override_action = "none"
-
-      visibility_config = {
-        cloudwatch_metrics_enabled = true
-        metric_name                = "AWSManagedRulesBotControlRuleSet-Metrics"
-        sampled_requests_enabled   = true
-      }
-
-      managed_rule_group_statement = {
-        name        = "AWSManagedRulesBotControlRuleSet"
-        vendor_name = "AWS"
-      }
-    },
-    # Not supported from Terraform yet => https://github.com/hashicorp/terraform-provider-aws/issues/23287
-    # This RuleSet can be enabled and configured from AWS Console => https://aws.amazon.com/es/blogs/aws-spanish/como-habilitar-account-takeover-prevention-atp-para-proteger-mis-paginas-de-inicio-de-sesion/
-    # {
-    #   name     = "AWSManagedRulesATPRuleSetByAWS"
-    #   priority = "5"
-
-    #   override_action = "none"
-
-    #   visibility_config = {
-    #     cloudwatch_metrics_enabled = true
-    #     metric_name                = "AWSManagedRulesATPRuleSet"
-    #     sampled_requests_enabled   = true
-    #   }
-
-    #   managed_rule_group_statement = {
-    #     name        = "AWSManagedRulesATPRuleSet"
-    #     vendor_name = "AWS"
-    #   }
-    # },
-    ### IP Rate Based Rule example
-    {
-      name     = "CustomRulesIpRateLimitBasedRuleSet"
-      priority = "7"
-
-      action = "count"
-
-      visibility_config = {
-        cloudwatch_metrics_enabled = true
-        metric_name                = "CustomRuleIpRateLimitBasedRuleSet-Metrics"
-        sampled_requests_enabled   = true
-      }
-
-      rate_based_statement = {
-        limit              = 100
-        aggregate_key_type = "IP"
-      }
-
-      # Optional scope_down_statement to refine what gets rate limited
-      # scope_down_statement = {
-      #   not_statement = { # not statement to rate limit everything except the following path
-      #     byte_match_statement = {
-      #       field_to_match = {
-      #         uri_path = "{}"
-      #       }
-      #       positional_constraint = "STARTS_WITH"
-      #       search_string         = "/path/to/match"
-      #       priority              = 0
-      #       type                  = "NONE"
-      #     }
-      #   }
-      # }
     },
   ]
 
