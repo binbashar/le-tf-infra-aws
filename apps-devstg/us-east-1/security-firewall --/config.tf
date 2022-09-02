fd9dfa91cb54952c5d9771291480564f946f4f37
Mariano Di Modugno <35702375+marianod92@users.noreply.github.com>
2022-09-02 18:12:13 -0300

    Feature | Security WAF with demo ALB (#428)
    * Update WAFv2 Rules and Configurations based on the recommendations for OWASP Top 10
    
    * Add configuration for create an example ALB and enabled/disable attachment with WAF for end-to-end demo test by .tfvars file
    
    * Update WAFv2 README.md

--- apps-devstg/us-east-1/security-firewall --/config.tf
+++ apps-devstg/us-east-1/security-firewall --/config.tf
@@ -6,11 +6,24 @@ provider "aws" {
   profile = var.profile
 }
 
+# Here we need a different AWS provider because ACM certificates
+# DNS validation records needs to be created in binbash-shared account
+#
+# binbash-shared route53 cross-account ACM dns validation update
+#
+provider "aws" {
+  region  = var.region
+  profile = "${var.project}-shared-devops"
+  //  profile                 = var.profile_shared
+  shared_credentials_file = "~/.aws/bb/config"
+  alias                   = "shared-route53"
+}
+
 #=============================#
 # Backend Config (partial)    #
 #=============================#
 terraform {
-  required_version = ">= 0.14.11"
+  required_version = "~> 1.1.3"
 
   required_providers {
     aws = "~> 4.10.0"
@@ -18,5 +31,42 @@ terraform {
 
   backend "s3" {
     key = "apps-devstg/security-firewall/terraform.tfstate"
+  }
+}
+
+#=============================#
+# Data sources                #
+#=============================#
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/network/terraform.tfstate"
+  }
+}
+
+
+data "terraform_remote_state" "certificates" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/security-certs/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "dns-shared" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = "${var.project}-shared-devops"
+    bucket  = "${var.project}-shared-terraform-backend"
+    key     = "shared/dns/binbash.com.ar/terraform.tfstate"
   }
 }
