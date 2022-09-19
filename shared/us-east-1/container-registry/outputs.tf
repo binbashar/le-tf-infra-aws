33a5b37c091c2a956db03258e85050bfcbd76a20
Angelo Fenoglio <angelo.fenoglio@binbash.com.ar>
2022-09-19 11:13:22 -0300

    Remove container-registry layer output

--- shared/us-east-1/container-registry/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "registry_url" {
-    description = "ECR registry URL"
-    value       = values(module.ecr_repositories)[0]["registry_url"]#split("/", values(module.ecr_repositories)[0]["repository_url"])[0]
-}
