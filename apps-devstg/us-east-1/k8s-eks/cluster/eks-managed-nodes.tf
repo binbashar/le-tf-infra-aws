1d3e08f1aa1f8f77f15e886ffb7b4d8fa4bcb431
Mariano Di Modugno <35702375+marianod92@users.noreply.github.com>
2022-09-29 15:42:41 -0300

    Enhancement | Upgrade node-exporter version and add Security Group Rules (#437)
    * Upgrade node-exporter version and add rules required for Prometheus/Grafana metrics
    * Upgrade node-exporter version

--- apps-devstg/us-east-1/k8s-eks/cluster/eks-managed-nodes.tf
+++ apps-devstg/us-east-1/k8s-eks/cluster/eks-managed-nodes.tf
@@ -73,6 +73,30 @@ module "cluster" {
       type                          = "ingress"
       source_cluster_security_group = true
     },
+    ingress_alb_ingress_metrics_tcp = {
+      description                   = "Cluster API to ALB Ingress Metrics"
+      protocol                      = "tcp"
+      from_port                     = 8080
+      to_port                       = 8080
+      type                          = "ingress"
+      source_cluster_security_group = true
+    },
+    ingress_certmanager_metrics_tcp = {
+      description                   = "Cluster API to CertManager Metrics"
+      protocol                      = "tcp"
+      from_port                     = 9402
+      to_port                       = 9402
+      type                          = "ingress"
+      source_cluster_security_group = true
+    },
+    ingress_node_exporter_metrics_tcp = {
+      description                   = "Cluster API to Node Exporter Metrics"
+      protocol                      = "tcp"
+      from_port                     = 9100
+      to_port                       = 9100
+      type                          = "ingress"
+      source_cluster_security_group = true
+    },
     #
     # DNS communication with the Internet
     #
