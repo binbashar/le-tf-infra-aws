a272153c7cd93c555e04ad64ca269bcced7a64a6
Angelo Fenoglio <angelo.fenoglio@binbash.com.ar>
2022-10-04 19:15:54 -0300

    Use Raw helm chart to deploy ClusterSecretStores

--- apps-devstg/us-east-1/k8s-eks/k8s-components/security.tf
+++ apps-devstg/us-east-1/k8s-eks/k8s-components/security.tf
@@ -69,67 +69,62 @@ resource "helm_release" "external_secrets" {
   ]
 }
 
-# These resources below (cluster_secrets_manager and cluster_parameter_store) need to be commented out and applied in a second step
-# The reason behind this can be found in this issue: https://github.com/hashicorp/terraform-provider-kubernetes/issues/1367#issuecomment-1239205722
-# and the surounding discussion.
-# TODO: Move onto using a raw YAML helm chart as in https://github.com/itscontained/charts/tree/master/itscontained/raw
+resource "helm_release" "cluster_secrets_manager" {
+  count = var.enable_external_secrets ? 1 : 0
 
-# resource "kubernetes_manifest" "cluster_secrets_manager" {
-#   count = var.enable_external_secrets ? 1 : 0
+  name       = "cluster-secrets-manager"
+  repository = "https://binbashar.github.io/helm-charts/"
+  chart      = "raw"
+  version    = "0.1.0"
+  values = [
+    <<-EOF
+    resources:
+      - apiVersion: external-secrets.io/v1beta1
+        kind: ClusterSecretStore
+        metadata:
+          name: cluster-secrets-manager
+        spec:
+          provider:
+            aws:
+              service: SecretsManager
+              region: ${var.region}
+              auth:
+                jwt:
+                  serviceAccountRef:
+                    name: external-secrets
+                    namespace: external-secrets
+    EOF
+  ]
 
-#   manifest = {
-#     "apiVersion" = "external-secrets.io/v1beta1"
-#     "kind"       = "ClusterSecretStore"
-#     "metadata" = {
-#       "name" = "cluster-secrets-manager"
-#     }
-#     "spec" = {
-#       "provider" = {
-#         "aws" = {
-#           "service" = "SecretsManager"
-#           "region"  = var.region
-#           "auth"    = {
-#             "jwt" = {
-#               "serviceAccountRef" = {
-#                 "name"      = "external-secrets",
-#                 "namespace" = "external-secrets"
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
+  depends_on = [helm_release.external_secrets[0]]
+}
 
-#   depends_on = [helm_release.external_secrets[0]]
-# }
+resource "kubernetes_manifest" "cluster_parameter_store" {
+  count = var.enable_external_secrets ? 1 : 0
 
-# resource "kubernetes_manifest" "cluster_parameter_store" {
-#   count = var.enable_external_secrets ? 1 : 0
+  name       = "cluster-parameter-store"
+  repository = "https://binbashar.github.io/helm-charts/"
+  chart      = "raw"
+  version    = "0.1.0"
+  values = [
+    <<-EOF
+    resources:
+      apiVersion: external-secrets.io/v1beta1
+      kind: ClusterSecretStore
+      metadata:
+        name: cluster-parameter-store
+      spec:
+        provider:
+          aws:
+            service: ParameterStore
+            region: ${var.region}
+            auth:
+              jwt:
+                serviceAccountRef:
+                  name: external-secrets
+                  namespace: external-secrets
+    EOF
+  ]
 
-#   manifest = {
-#     "apiVersion" = "external-secrets.io/v1beta1"
-#     "kind"       = "ClusterSecretStore"
-#     "metadata" = {
-#       "name" = "cluster-parameter-store"
-#     }
-#     "spec" = {
-#       "provider" = {
-#         "aws" = {
-#           "service" = "ParameterStore"
-#           "region"  = var.region
-#           "auth"    = {
-#             "jwt" = {
-#               "serviceAccountRef" = {
-#                 "name"      = "external-secrets",
-#                 "namespace" = "external-secrets"
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-
-#   depends_on = [helm_release.external_secrets[0]]
-# }
+  depends_on = [helm_release.external_secrets[0]]
+}
