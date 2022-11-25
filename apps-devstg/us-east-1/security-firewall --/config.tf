93bcc4b14c671a79785b89195f096097e15af968
Mariano G. Di Modugno <35702375+marianod92@users.noreply.github.com>
2022-11-25 17:26:01 -0300

    Add configuration required for AWS WAFv2 logging (#449)
    * Add configuration required for AWS WAFv2 logging
    * Update Terraform Version, AWS Terraform provider and build.env with leverage-cli latest version
    * Update build.env to new config required for leverage-cli >= 1.8.0 for WAFv2 & GuardDuty

--- apps-devstg/us-east-1/security-firewall --/config.tf
+++ apps-devstg/us-east-1/security-firewall --/config.tf
@@ -23,10 +23,10 @@ provider "aws" {
 # Backend Config (partial)    #
 #=============================#
 terraform {
-  required_version = "~> 1.1.3"
+  required_version = "~> 1.2.7"
 
   required_providers {
-    aws = "~> 4.10.0"
+    aws = "~> 4.40.0"
   }
 
   backend "s3" {
--- security/us-east-1/security-monitoring/config.tf
+++ security/us-east-1/security-monitoring/config.tf
@@ -23,10 +23,10 @@ provider "vault" {
 # Backend Config (partial)    #
 #=============================#
 terraform {
-  required_version = ">= 1.1.3"
+  required_version = ">= 1.2.7"
 
   required_providers {
-    aws   = "~> 4.31.0"
+    aws   = "~> 4.40.0"
     vault = "~> 2.18.0"
   }
 
