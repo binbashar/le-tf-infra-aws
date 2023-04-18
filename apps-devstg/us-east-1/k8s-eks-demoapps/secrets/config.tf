5d0288cab4933eba195cd01128757a1791f92fed
marianod92 <mariano.dimodugno@binbash.com.ar>
2023-04-18 15:40:38 -0300

    Add secrets leyer for k8s-eks-demoapps (WIP)

---
+++ apps-devstg/us-east-1/k8s-eks-demoapps/secrets/config.tf
@@ -0,0 +1,59 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+}
+
+#=============================#
+# Backend Config (partial)    #
+#=============================#
+terraform {
+  required_version = ">= 1.1.3"
+
+  required_providers {
+    aws = "~> 4.10"
+  }
+
+  backend "s3" {
+    key = "apps-devstg/k8s-eks-demoapps/secrets/terraform.tfstate"
+  }
+}
+
+#=============================#
+# Data sources                #
+#=============================#
+data "terraform_remote_state" "keys" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/security-keys/terraform.tfstate"
+  }
+}
+# Note: Commented out while database is not deployed
+# data "terraform_remote_state" "apps-devstg-pgsql" {
+#   backend = "s3"
+
+#   config = {
+#     region  = var.region
+#     profile = var.profile
+#     bucket  = var.bucket
+#     key     = "${var.environment}/databases-pgsql/terraform.tfstate"
+#   }
+# }
+
+# Note: Commented out while database is not deployed
+# data "terraform_remote_state" "apps-devstg-aurora-pgsql" {
+#   backend = "s3"
+
+#   config = {
+#     region  = var.region
+#     profile = var.profile
+#     bucket  = var.bucket
+#     key     = "${var.environment}/databases-aurora-pgsql/terraform.tfstate"
+#   }
+# }
---
+++ apps-devstg/us-east-1/k8s-eks-demoapps/secrets/secrets.tf
@@ -0,0 +1,39 @@
+module "secrets" {
+  source = "github.com/binbashar/terraform-aws-secrets-manager.git?ref=0.7.0"
+
+  secrets = {
+    "/repositories/k8s-eks-demoapps" = {
+      description             = "Repository: Google Microservices DemoApp - Deploy Key"
+      recovery_window_in_days = 7
+      secret_string           = "PLACEHOLDER"
+      kms_key_id              = data.terraform_remote_state.keys.outputs.aws_kms_key_id
+    },
+  }
+
+  tags = local.tags
+
+}
+
+# Set secrets policies
+data "aws_iam_policy_document" "secret_policy" {
+  statement {
+    sid       = "GetSecret"
+    effect    = "Allow"
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = ["*"]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${var.accounts.apps-devstg.id}:role/DevOps",
+        # "arn:aws:iam::${var.accounts.management.id}:role/OrganizationAccountAccessRole"
+      ]
+
+    }
+  }
+}
+
+resource "aws_secretsmanager_secret_policy" "secrets_policy" {
+  for_each   = module.secrets.secret_arns
+  secret_arn = each.value
+  policy     = data.aws_iam_policy_document.secret_policy.json
+}
