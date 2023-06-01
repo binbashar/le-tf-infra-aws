0dbab28a578eb17f27199d19b3a043f8f4198581
Diego OJ <diego.ojeda@binbash.com.ar>
2023-06-01 19:11:31 -0300

    Fix EKS DemoApps Add-ons implementation which was failing at cluster bootstrap

--- apps-devstg/us-east-1/k8s-eks-demoapps/cluster/locals.tf
+++ apps-devstg/us-east-1/k8s-eks-demoapps/cluster/locals.tf
@@ -27,4 +27,35 @@ locals {
       groups   = ["system:masters"]
     },
   ]
+
+  # ---------------------------------------------------------------------------
+  # IMPORTANT
+  # ---------------------------------------------------------------------------
+  # If you plan to use EKS managed add-ons keep in mind that some add-ons rely
+  # on IAM roles which need to be created/updated for them to work. Said roles
+  # are defined in the "identities" layer which needs to be applied only after
+  # the cluster is up and running. After that you should be able to install the
+  # managed add-ons on the cluster.
+  # ---------------------------------------------------------------------------
+  addons_available = {
+    coredns = {
+      addon_version     = "v1.8.7-eksbuild.4"
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {
+      addon_version     = "v1.22.17-eksbuild.2"
+      resolve_conflicts = "OVERWRITE"
+    }
+    vpc-cni = {
+      addon_version            = "v1.12.6-eksbuild.2"
+      resolve_conflicts        = "OVERWRITE"
+      service_account_role_arn = data.terraform_remote_state.cluster-identities.outputs.eks_addons_vpc_cni
+    }
+    aws-ebs-csi-driver = {
+      addon_version            = "v1.18.0-eksbuild.1"
+      resolve_conflicts        = "OVERWRITE"
+      service_account_role_arn = data.terraform_remote_state.cluster-identities.outputs.eks_addons_ebs_csi
+    }
+  }
+  addons_enabled = var.use_managed_addons ? local.addons_available : {}
 }
