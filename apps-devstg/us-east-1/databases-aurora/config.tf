b98fd66f8392ee0e2c90147f185777fbd78d46c2
Ezequiel Godoy <ezequiel.godoy@binbash.com.ar>
2023-06-13 17:42:48 -0300

    Enhacement | RDS snapshot export s3

--- apps-devstg/us-east-1/databases-aurora/config.tf
+++ apps-devstg/us-east-1/databases-aurora/config.tf
@@ -29,7 +29,7 @@ provider "mysql" {
 # Backend Config (partial)    #
 #=============================#
 terraform {
-  required_version = "~> 1.1.3"
+  required_version = "~> 1.2"
 
   required_providers {
     aws   = "~> 4.12"
