c4c53d7b04f3c779db9aa0832ced5967f1d5c913
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2023-06-26 14:25:07 -0300

    fixing vpc-apps-prd remote state key wrongly pointing to inxisting k8s-eks/network layer

--- shared/global/base-dns/binbash.co/config.tf
+++ shared/global/base-dns/binbash.co/config.tf
@@ -137,6 +137,6 @@ data "terraform_remote_state" "vpc-apps-prd" {
     region  = var.region
     profile = "${var.project}-apps-prd-devops"
     bucket  = "${var.project}-apps-prd-terraform-backend"
-    key     = "apps-prd/k8s-eks/network/terraform.tfstate"
+    key     = "apps-prd/network/terraform.tfstate"
   }
 }
