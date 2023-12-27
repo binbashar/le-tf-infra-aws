46f200dd2f904c25d8ed6c572b97d34e5fcb85ea
Juan Matias Kungfoo de la Camara Beovide <juan.delacamara@binbash.com.ar>
2023-12-27 10:45:20 -0300

    FIX/ fixed terraform format

--- shared/us-east-1/tools-cloud-scheduler-stop-start/outputs.tf
+++ shared/us-east-1/tools-cloud-scheduler-stop-start/outputs.tf
@@ -1,9 +1,9 @@
 output "start_http_endpoint" {
   description = "The http endpoint to trigger start"
-  value = module.schedule_ec2_start_daily_morning.http_trigger
+  value       = module.schedule_ec2_start_daily_morning.http_trigger
 }
 
 output "stop_http_endpoint" {
   description = "The http endpoint to trigger stop"
-  value = module.schedule_ec2_stop_daily_midnight.http_trigger
+  value       = module.schedule_ec2_stop_daily_midnight.http_trigger
 }
