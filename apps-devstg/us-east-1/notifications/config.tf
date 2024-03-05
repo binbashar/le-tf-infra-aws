707ff776feca204a18569b514a72a36f7c594e43
Juan Matias Kungfoo de la Camara Beovide <juan.delacamara@binbash.com.ar>
2024-03-05 12:20:37 -0300

    FEATURE/ added sops for urls

--- apps-devstg/us-east-1/notifications/config.tf
+++ apps-devstg/us-east-1/notifications/config.tf
@@ -7,27 +7,17 @@ provider "aws" {
 }
 
 #=============================#
-# Vault Provider Settings     #
-#=============================#
-provider "vault" {
-  address = var.vault_address
-
-  /*
-  Vault token that will be used by Terraform to authenticate.
- admin token from https://portal.cloud.hashicorp.com/.
- */
-  token = var.vault_token
-}
-
-#=============================#
 # Backend Config (partial)    #
 #=============================#
 terraform {
-  required_version = "~> 1.2.7"
+  required_version = "~> 1.3"
 
   required_providers {
     aws   = "~> 4.10"
-    vault = "~> 3.6.0"
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 0.7"
+    }
   }
 
   backend "s3" {
@@ -53,6 +43,6 @@ data "terraform_remote_state" "keys" {
   }
 }
 
-data "vault_generic_secret" "slack_hook_url_monitoring" {
-  path = "secrets/${var.project}/${var.environment}/notifications"
+data "sops_file" "secrets" {
+  source_file = "secrets.enc.yaml"
 }
