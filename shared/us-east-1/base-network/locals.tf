b5322de7b241ee492118bbb0fe130414013d7dd6
Diego OJ <diego.ojeda@binbash.com.ar>
2024-03-05 22:59:41 -0300

    Remove EKS v1.17 and adjust Shared network accordingly

--- shared/us-east-1/base-network/locals.tf
+++ shared/us-east-1/base-network/locals.tf
@@ -129,12 +129,6 @@ locals {
       bucket  = "${var.project}-apps-devstg-terraform-backend"
       key     = "apps-devstg/network/terraform.tfstate"
     }
-    apps-devstg-k8s-eks-v117 = {
-      region  = var.region
-      profile = "${var.project}-apps-devstg-devops"
-      bucket  = "${var.project}-apps-devstg-terraform-backend"
-      key     = "apps-devstg/k8s-eks-v1.17/network/terraform.tfstate"
-    }
     apps-devstg-k8s-eks = {
       region  = var.region
       profile = "${var.project}-apps-devstg-devops"
