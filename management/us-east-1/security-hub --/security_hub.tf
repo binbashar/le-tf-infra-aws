b62d7de299249df7995b1d85370d110b9c15b40c
Martin galeano <extmgaleano@transbank.cl>
2024-06-04 11:17:28 -0300

    Update sec hub readme

--- management/us-east-1/security-hub --/security_hub.tf
+++ management/us-east-1/security-hub --/security_hub.tf
@@ -1,4 +1,4 @@
-## Security Hub is administered via the Security Account, 
+## Security Hub is administered via the Security Account,
 ## which is designated as the administrator by the Management Account.
 ## Enable the security standards that Security Hub has designated as default:
 ## AWS Foundational Security Best Practices v1.0.0 and CIS AWS Foundations Benchmark v1.2.0
