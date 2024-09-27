0d88043237ecf41c91994fa0dce23f40d97009b4
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2024-09-27 15:37:18 -0300

    data-science data-lake reference architecture

---
+++ data-science/us-east-1/databases-aurora-mysql/cluster_demoapps.tf
@@ -0,0 +1,66 @@
+module "demoapps" {
+  source = "github.com/binbashar/terraform-aws-rds-aurora.git?ref=v7.2.2"
+
+  # General settings
+  name           = "${var.project}-${var.environment}-binbash-aurora-mysql"
+  engine         = "aurora-mysql"
+  engine_mode    = "provisioned"
+  engine_version = "5.7"
+
+  # Initial database and credentials
+  database_name          = "demoapps"
+  master_username        = "admin"
+  master_password        = data.vault_generic_secret.databases_aurora.data["administrator_password"]
+  create_random_password = false
+
+  # VPC and Subnets
+  vpc_id  = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnets = data.terraform_remote_state.vpc.outputs.private_subnets
+
+  # Instance type and desired instances
+  instance_class = "db.t3.small"
+  instances = {
+    one = {}
+  }
+
+
+  # Autoscaling settings
+  autoscaling_enabled = false
+  # autoscaling_min_capacity        = 1
+  # autoscaling_max_capacity         = 3
+  # autoscaling_target_cpu         = 85
+  # autoscaling_target_connections = 200
+
+  # Storage encrypted as default
+  storage_encrypted = true
+
+  # Determines whether or not any DB modifications are applied immediately, or during the maintenance window
+  # Only 'true' in test environments
+  apply_immediately = true
+
+  # Automatic backup settings
+  backup_retention_period = 1
+  preferred_backup_window = "14:00-15:00"
+
+  # This avoid a snapshot before destroy the cluster
+  skip_final_snapshot = true
+
+  # Monitoring settings
+  # enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
+
+  # Database parameters: you can specify your own if you must
+  # db_parameter_group_name         = aws_db_parameter_group.aurora_db_57_parameter_group.id
+  # db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.aurora_57_cluster_parameter_group.id
+
+  # If true, must add policy to iam auth (user or role)
+  iam_database_authentication_enabled = false
+
+  # Security group settings
+  create_security_group = true
+  allowed_cidr_blocks = [
+    data.terraform_remote_state.eks_vpc_demoapps.outputs.vpc_cidr_block,
+    data.terraform_remote_state.shared_vpc.outputs.vpc_cidr_block
+  ]
+
+  tags = local.tags
+}
---
+++ data-science/us-east-1/databases-aurora-mysql/config.tf
@@ -0,0 +1,79 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+}
+
+provider "mysql" {
+  endpoint = module.demoapps.cluster_endpoint
+  username = module.demoapps.cluster_master_username
+  password = module.demoapps.cluster_master_password
+}
+
+#=============================#
+# Backend Config (partial)    #
+#=============================#
+terraform {
+  required_version = "~> 1.5"
+
+  required_providers {
+    aws   = "~> 4.12"
+    mysql = {
+      source  = "winebarrel/mysql"
+      version = "1.10.6"
+    }
+  }
+
+  backend "s3" {
+    key = "data-science/databases-aurora-mysql/terraform.tfstate"
+  }
+}
+
+#=============================#
+# Data sources                #
+#=============================#
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/network/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "eks_vpc_demoapps" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = var.profile
+    bucket  = var.bucket
+    key     = "${var.environment}/k8s-eks-demoapps/network/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "shared_vpc" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = "${var.project}-shared-devops"
+    bucket  = "${var.project}-shared-terraform-backend"
+    key     = "shared/network/terraform.tfstate"
+  }
+}
+
+#
+# Note: for the sake of simplicity we are storing the db admin credentials
+#       under the same path of a demoapp. In other words, demoapps will use
+#       use admin credentials for talking to the db. Later on, we will have
+#       to store admin credentials under a separate path and create separate,
+#       more restrictied credentials for demoapps.
+#
+data "vault_generic_secret" "databases_aurora" {
+  path = "secrets/${var.project}/${var.environment}/databases-aurora"
+}
