f77d360e6a25ad8edccfd10567e74f41d2665990
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2024-10-28 10:19:15 -0300

    498: Refactor CloudTrail (#647)
    * Remove all trails from all accounts
    
    * Create a multi-region, organizational trail in Security, as delegated admin
    
    * Enable CloudTrail API Alarms and improve documentation a bit

--- management/global/organizations/config.tf
+++ management/global/organizations/config.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = "~> 1.2"
 
   required_providers {
-    aws = "~> 4.0"
+    aws = "~> 5.0"
   }
 
   backend "s3" {
--- security/us-east-1/security-audit/cloudtrail.tf
+++ security/us-east-1/security-audit/cloudtrail.tf
@@ -1,60 +1,76 @@
+#
+# Create a centralized, multi-region, organizational trail in the Security account.
+# IMPORTANT: before you can enable this, you must delegate the administration to
+# the Security account from the Management account.
+#
 module "cloudtrail" {
-  source                        = "github.com/binbashar/terraform-aws-cloudtrail.git?ref=0.20.1"
-  namespace                     = var.project
-  stage                         = var.environment
-  name                          = "cloudtrail-org"
-  enable_logging                = true
-  enable_log_file_validation    = true
+  source                        = "github.com/binbashar/terraform-aws-cloudtrail.git?ref=0.24.0"
+  name                          = "${var.project}-${var.environment}-cloudtrail-org"
+
+  # Include global services such as Route 53 or IAM
   include_global_service_events = true
   is_multi_region_trail         = true
+  is_organization_trail         = true
+
+  # Enable to S3 and CloudWatch Logs (and store log validation files)
+  enable_logging                = true
+  enable_log_file_validation    = true
+
+  # Send event logs to S3
   s3_bucket_name                = module.cloudtrail_s3_bucket.bucket_id
+  kms_key_arn                   = data.terraform_remote_state.keys.outputs.aws_kms_key_arn
+
+  # Enable for API alarms
   cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
   cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_cloudwatch_events.arn
-  kms_key_arn                   = data.terraform_remote_state.keys.outputs.aws_kms_key_arn
-  #is_organization_trail        = true
 }
 
+#
+# Create an S3 bucket for storing CloudTrail event logs.
+# This is typically used for setting a longer retention period than the 90
+# days that CloudTrail provides by default.
+#
 module "cloudtrail_s3_bucket" {
   source                 = "github.com/binbashar/terraform-aws-cloudtrail-s3-bucket.git?ref=0.26.4"
-  namespace              = var.project
-  stage                  = var.environment
-  name                   = "cloudtrail-org"
+  name                   = "${var.project}-${var.environment}-cloudtrail-org"
   lifecycle_rule_enabled = var.lifecycle_rule_enabled
   versioning_enabled     = true
-  #
-  # NOTE: Had to pass null here because there seems to be an issue with the
-  #       module which is trying to set tags to lifecycle policies
-  #
-  lifecycle_tags = null
-
-  #
-  # NOTE: this actually isn't supported by the module. The issue is reported
-  # here: https://github.com/cloudposse/terraform-aws-cloudtrail-s3-bucket/issues/19
-  #
-  # policy          = data.aws_iam_policy_document.cloudtrail_s3_bucket.json
-  acl             = "private"
-  expiration_days = 120
+  acl                    = "private"
+  expiration_days        = 120
+  tags                   = local.tags
 }
 
+#
+# Set up CloudWatch Alarms based on specific CloudTrail events.
+# Refer to the file "metrics.auto.tfvars" to view the list of alarms and their specs.
+#
 module "cloudtrail_api_alarms" {
   source            = "github.com/binbashar/terraform-aws-cloudtrail-cloudwatch-alarms.git?ref=0.14.3"
+
+  # The log group whose logs will be used for configuring metric filters and alarms
   log_group_region  = var.region
   log_group_name    = aws_cloudwatch_log_group.cloudtrail.name
+
+  # The custom metrics that will be created via metric filters
+  metrics           = local.metrics
+
+  # The namespace under which the custom metrics will live
   metric_namespace  = var.metric_namespace
+
+  # Whether to enable a custom dashboard using the custom metrics that will be created
   dashboard_enabled = var.create_dashboard
 
-  # Uncomment if /notifications SNS is configured and you want to send notifications via slack
-  sns_topic_arn = data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring_sec
-  metrics       = local.metrics
+  # Pass a custom SNS topic that will be hooked to the alarms that the module will create,
+  # otherwise the module will use its own topic
+  sns_topic_arn     = data.terraform_remote_state.notifications.outputs.sns_topic_arn_monitoring_sec
 
-  # KMS key use for encrypting the Amazon SNS topic.
+  # A KMS key that will be used for encrypting messages writtent to the SNS topic
   kms_master_key_id = data.terraform_remote_state.keys.outputs.aws_kms_key_id
-
 }
 
-#==================================================================#
-# setup cloudwatch logs group in order to receive cloudtrail events
-#==================================================================#
+#
+# Set up a CloudWatch log group to receive CloudTrail events
+#
 resource "aws_cloudwatch_log_group" "cloudtrail" {
   name              = "${var.project}-${var.environment}-cloudtrail"
   retention_in_days = "14"
@@ -63,9 +79,9 @@ resource "aws_cloudwatch_log_group" "cloudtrail" {
   tags = local.tags
 }
 
-#==================================================================#
-# setup role and policy to allow cloudtrail to write to cloudwatch
-#==================================================================#
+#
+# Create a role & policy for CloudTrail to write its logs to CloudWatch logs
+#
 resource "aws_iam_role" "cloudtrail_cloudwatch_events" {
   name               = "CloudtrailCloudwatchEvents"
   assume_role_policy = data.aws_iam_policy_document.assume_policy.json
@@ -91,18 +107,16 @@ resource "aws_iam_role_policy" "cloudtrail_cloudwatch_events_policy" {
 
 data "aws_iam_policy_document" "cloudtrail_role_policy" {
   statement {
-    effect  = "Allow"
-    actions = ["logs:CreateLogStream"]
-
+    effect    = "Allow"
+    actions   = [ "logs:CreateLogStream" ]
     resources = [
       "arn:aws:logs:${var.region}:${var.accounts.security.id}:log-group:${aws_cloudwatch_log_group.cloudtrail.name}:log-stream:*",
     ]
   }
 
   statement {
-    effect  = "Allow"
-    actions = ["logs:PutLogEvents"]
-
+    effect    = "Allow"
+    actions   = [ "logs:PutLogEvents" ]
     resources = [
       "arn:aws:logs:${var.region}:${var.accounts.security.id}:log-group:${aws_cloudwatch_log_group.cloudtrail.name}:log-stream:*",
     ]
