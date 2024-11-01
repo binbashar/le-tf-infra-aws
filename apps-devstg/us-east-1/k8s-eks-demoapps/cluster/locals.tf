0b58e484a10fd49f66fc3068ff9fc40cf0706cd4
Juan Matias Kungfoo de la Camara Beovide <juan.delacamara@binbash.com.ar>
2024-11-01 08:57:25 -0300

    Propagating terraform module changes to demoapps cluster

--- apps-devstg/us-east-1/k8s-eks-demoapps/cluster/locals.tf
+++ apps-devstg/us-east-1/k8s-eks-demoapps/cluster/locals.tf
@@ -5,13 +5,9 @@ locals {
     Project     = var.project
   }
 
-  # Additional AWS account numbers to add to the aws-auth configmap
-  map_accounts = []
-
-  # Additional IAM users to add to the aws-auth configmap. See examples/basic/variables.tf for example format
-  map_users = []
-
-  # Additional IAM roles to add to the aws-auth configmap.
+  #
+  # Configure which IAM roles can have access to K8S API
+  #
   map_roles = [
     #
     # Allow DevOps role to become cluster admins
@@ -21,27 +17,51 @@ locals {
       username = "DevOps"
       groups   = ["system:masters"]
     },
+    #
+    # Allow DevOps (SSO) role to become cluster admins
+    #
     {
       rolearn  = "arn:aws:iam::${var.accounts.apps-devstg.id}:role/AWSReservedSSO_DevOps_2b78d1d8a7818ab3"
       username = "DevOps"
       groups   = ["system:masters"]
     },
+    #
+    # Allow Developer role access for specific Namespaces (Role/RoleBinding)
+    #
+    {
+      rolearn  = "arn:aws:iam::${var.accounts.apps-devstg.id}:role/Developer"
+      username = "Developer"
+      groups   = ["system:authenticated"]
+    },
   ]
 
-  # ---------------------------------------------------------------------------
-  # IMPORTANT
-  # ---------------------------------------------------------------------------
-  # If you plan to use EKS managed add-ons keep in mind that some add-ons rely
-  # on IAM roles which need to be created/updated for them to work. Said roles
-  # are defined in the "identities" layer which needs to be applied only after
-  # the cluster is up and running. You can orchestrate that execution in that
-  # order by toggling the "use_managed_addons" variable in "variables.tf".
-  # Said execution order should go as follows:
-  #   1. Apply this layer
-  #   2. Apply the identities layers
-  #   3. Enable the "use_managed_addons" variable and apply this layer again
-  # ---------------------------------------------------------------------------
-  addons_available = {
+  # To change the default node group use these vars:
+  #
+  # node_group_min_size
+  # node_group_max_size
+  # node_group_desired_size
+  # node_group_instance_types
+  # node_group_capacity_type
+  #
+  # For additional node groups complete the following object like this one:
+  #
+  #additional_node_groups = {
+  #  monitoring = {
+  #    min_size       = 1,
+  #    max_size       = 3,
+  #    desired_size   = 1,
+  #    instance_types = ["t3a.medium"],
+  #    subnet_ids = data.terraform_remote_state.eks-vpc.outputs.private_subnets
+  #    labels     = merge(local.tags, { "stack" = "monitoring" })
+  #    taints = {
+  #      dedicated_monitoring = {
+  #        key    = "stack"
+  #        value  = "monitoring"
+  #        effect = "NO_SCHEDULE"
+  #      }
+  #    }
+  #  }
+  #}
+  additional_node_groups = {
   }
-  addons_enabled = var.use_managed_addons ? local.addons_available : {}
 }
--- apps-devstg/us-east-1/k8s-eks-demoapps/network/config.tf
+++ apps-devstg/us-east-1/k8s-eks-demoapps/network/config.tf
@@ -16,10 +16,10 @@ provider "aws" {
 # Backend Config (partial)
 #
 terraform {
-  required_version = "~> 1.2"
+  required_version = "~> 1.3"
 
   required_providers {
-    aws = "~> 4.11"
+    aws = "~> 5.20"
   }
 
   backend "s3" {
@@ -27,85 +27,9 @@ terraform {
   }
 }
 
-#
-# Data sources
-#
-
-# TGW
-data "terraform_remote_state" "tgw" {
-  count = var.enable_tgw ? 1 : 0
-
-  backend = "s3"
-
-  config = {
-    region  = var.region
-    profile = "${var.project}-network-devops"
-    bucket  = "${var.project}-network-terraform-backend"
-    key     = "network/transit-gateway/terraform.tfstate"
-  }
-}
-
-data "terraform_remote_state" "tools-vpn-server" {
-  backend = "s3"
-
-  config = {
-    region  = var.region
-    profile = "${var.project}-shared-devops"
-    bucket  = "${var.project}-shared-terraform-backend"
-    key     = "shared/vpn-server/terraform.tfstate"
-  }
-}
-#
-# VPC remote states for network
-data "terraform_remote_state" "network-vpcs" {
-  for_each = local.network-vpcs
-
-  backend = "s3"
-
-  config = {
-    region  = lookup(each.value, "region")
-    profile = lookup(each.value, "profile")
-    bucket  = lookup(each.value, "bucket")
-    key     = lookup(each.value, "key")
-  }
-}
-
-# VPC remote states for shared
-data "terraform_remote_state" "shared-vpcs" {
-
-  for_each = local.shared-vpcs
-
-  backend = "s3"
-
-  config = {
-    region  = lookup(each.value, "region")
-    profile = lookup(each.value, "profile")
-    bucket  = lookup(each.value, "bucket")
-    key     = lookup(each.value, "key")
-  }
-}
-
-# VPC remote states for apps-devstg
-data "terraform_remote_state" "apps-devstg-vpcs" {
-
-  for_each = {
-    for k, v in local.apps-devstg-vpcs :
-    k => v if !v["tgw"]
-  }
-
-  backend = "s3"
-
-  config = {
-    region  = lookup(each.value, "region")
-    profile = lookup(each.value, "profile")
-    bucket  = lookup(each.value, "bucket")
-    key     = lookup(each.value, "key")
-  }
-}
 
 data "terraform_remote_state" "shared-dns" {
   backend = "s3"
-
   config = {
     region  = var.region
     profile = "${var.project}-shared-devops"
@@ -113,3 +37,4 @@ data "terraform_remote_state" "shared-dns" {
     key     = "shared/dns/binbash.com.ar/terraform.tfstate"
   }
 }
+
