4d8e4192df3581161b3549745ec0531591723888
Juan Matias Kungfoo de la Camara Beovide <juan.delacamara@binbash.com.ar>
2024-11-01 09:12:13 -0300

    Propagating terraform module changes to base cluster

--- apps-devstg/us-east-1/k8s-eks/cluster/config.tf
+++ apps-devstg/us-east-1/k8s-eks/cluster/config.tf
@@ -6,46 +6,32 @@ provider "aws" {
   profile = var.profile
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
 #
 # Backend Config (partial)
 #
 terraform {
-  required_version = ">= 1.2"
+  required_version = "~> 1.3"
 
   required_providers {
-    aws        = "~> 4.10"
-    kubernetes = "~> 2.10"
+    aws = "~> 5.20"
+    #kubernetes = "~> 2.10"
   }
 
   backend "s3" {
-    key = "apps-devstg/k8s-eks/cluster/terraform.tfstate"
+    key = "apps-devstg/k8s-eks-training/cluster/terraform.tfstate"
   }
 }
 
 #
 # Data Sources
 #
-data "aws_eks_cluster" "cluster" {
-  name = module.cluster.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.cluster.cluster_id
-}
-
 data "terraform_remote_state" "eks-vpc" {
   backend = "s3"
   config = {
     region  = var.region
     profile = var.profile
     bucket  = var.bucket
-    key     = "apps-devstg/k8s-eks/network/terraform.tfstate"
+    key     = "apps-devstg/k8s-eks-training/network/terraform.tfstate"
   }
 }
 
--- apps-devstg/us-east-1/k8s-eks/network/outputs.tf
+++ apps-devstg/us-east-1/k8s-eks/network/outputs.tf
@@ -1,7 +1,7 @@
 # VPC ID
 output "vpc_id" {
   description = "VPC ID"
-  value       = module.vpc-eks.vpc_id
+  value       = module.vpc.vpc_id
 }
 
 output "cluster_name" {
@@ -14,51 +14,63 @@ output "cluster_name" {
 #
 output "vpc_name" {
   description = "VPC Name"
-  value       = local.vpc_name
+  value       = module.vpc.vpc_name
 }
 
 output "vpc_cidr_block" {
   description = "VPC CIDR Block"
-  value       = local.vpc_cidr_block
+  value       = module.vpc.vpc_cidr_block
 }
 
 output "availability_zones" {
   description = "List of availability zones"
-  value       = local.azs
+  value       = module.vpc.azs
 }
 
 # Subnets
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = module.vpc-eks.private_subnets
+  value       = module.vpc.private_subnets
 }
 
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = module.vpc-eks.public_subnets
+  value       = module.vpc.public_subnets
 }
 
 output "private_subnets_cidr" {
   description = "List of IDs of private subnets"
-  value       = local.private_subnets_cidr
+  value       = module.vpc.private_subnets_cidr
 }
 
 output "public_subnets_cidr" {
   description = "List of IDs of public subnets"
-  value       = local.public_subnets_cidr
+  value       = module.vpc.public_subnets_cidr
 }
 
 output "nat_gateway_ids" {
   description = "NAT Gateway IDs"
-  value       = module.vpc-eks.natgw_ids
+  value       = module.vpc.natgw_ids
 }
 
 output "public_route_table_ids" {
   description = "List of IDs of public route tables"
-  value       = module.vpc-eks.public_route_table_ids
+  value       = module.vpc.public_route_table_ids
 }
 
 output "private_route_table_ids" {
   description = "List of IDs of private route tables"
-  value       = module.vpc-eks.private_route_table_ids
+  value       = module.vpc.private_route_table_ids
+}
+
+output "peerings_created_for" {
+  value = {for k, v in module.vpc.peerings_created_for : k => v}
+}
+
+output "z_note_on_vpn" {
+  value = <<EOF
+  # ##########################################################
+  Remember, if you have set a VPN, add the current CIDR (${module.vpc.vpc_cidr_block}) to its networks
+  # ##########################################################
+EOF
 }
