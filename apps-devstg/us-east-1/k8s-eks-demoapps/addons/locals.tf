804cb9bed93e379002bbc2cffecbe0c474c3060a
Angelo Fenoglio <angelo.fenoglio@binbash.com.ar>
2024-11-07 10:54:47 -0300

    Upgrade addons versions

--- apps-devstg/us-east-1/k8s-eks-demoapps/addons/locals.tf
+++ apps-devstg/us-east-1/k8s-eks-demoapps/addons/locals.tf
@@ -7,23 +7,26 @@ locals {
   }
   addons_available = {
     coredns = {
-      addon_version     = "v1.10.1-eksbuild.6"
-      resolve_conflicts = "OVERWRITE"
+      addon_version               = "v1.11.3-eksbuild.1"
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "OVERWRITE"
     }
     kube-proxy = {
-      addon_version     = "v1.28.4-eksbuild.4"
-      resolve_conflicts = "OVERWRITE"
+      addon_version               = "v1.31.1-eksbuild.2"
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "OVERWRITE"
     }
     vpc-cni = {
-      addon_version            = "v1.16.0-eksbuild.1"
-      resolve_conflicts        = "OVERWRITE"
-      service_account_role_arn = data.terraform_remote_state.cluster-identities.outputs.eks_addons_vpc_cni
+      addon_version               = "v1.18.5-eksbuild.1"
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "OVERWRITE"
+      service_account_role_arn    = data.terraform_remote_state.cluster-identities.outputs.eks_addons_vpc_cni
     }
     aws-ebs-csi-driver = {
-      addon_version            = "v1.23.0-eksbuild.1"
-      resolve_conflicts        = "OVERWRITE"
-      service_account_role_arn = data.terraform_remote_state.cluster-identities.outputs.eks_addons_ebs_csi
+      addon_version               = "v1.36.0-eksbuild.1"
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "OVERWRITE"
+      service_account_role_arn    = data.terraform_remote_state.cluster-identities.outputs.eks_addons_ebs_csi
     }
   }
-
 }
