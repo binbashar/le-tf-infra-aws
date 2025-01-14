cf55a1bbe7d0a3871d6476d052e66a263d60740a
Ezequiel Godoy <ezequiel.godoy@external.mercell.com>
2025-01-14 19:22:47 +0100

    small fixes

--- management/global/sso/client-vpn-application.tf
+++ management/global/sso/client-vpn-application.tf
@@ -7,7 +7,7 @@ resource "aws_ssoadmin_application" "client_vpn" {
 
   name                     = "${var.project}-client-vpn"
   application_provider_arn = "arn:aws:sso::aws:applicationProvider/custom-saml"
-  description              = "AWS Client VPN"
+  description              = "Awsome Clieny VPN"
   instance_arn             = tolist(data.aws_ssoadmin_instances.this[0].arns)[0]
 
   portal_options {
@@ -20,7 +20,7 @@ resource "aws_ssoadmin_application" "client_vpn" {
 }
 
 resource "aws_ssoadmin_application_assignment" "client_vpn" {
-  for_each = local.enable_sso_client_vpn == true ? toset(local.client_vpn_groups) : {}
+  for_each = local.enable_sso_client_vpn == true ? toset(local.client_vpn_groups) : toset([])
 
   application_arn = aws_ssoadmin_application.client_vpn[0].application_arn
   principal_id    = split("/", aws_identitystore_group.default[each.key].id)[1]
