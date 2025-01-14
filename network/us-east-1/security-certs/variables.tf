911405afc29fec19303f6d39ff7bccbc29bc231e
Ezequiel Godoy <ezequiel.godoy@external.mercell.com>
2025-01-14 19:44:15 +0100

    add cert

---
+++ network/us-east-1/security-certs/variables.tf
@@ -0,0 +1,8 @@
+#=============================#
+# ACM DNS variables           #
+#=============================#
+variable "profile_shared" {
+  type        = string
+  description = "Shared account aws iam profile in order to update Route53 DNS service"
+  default     = "bb-shared-devops"
+}
