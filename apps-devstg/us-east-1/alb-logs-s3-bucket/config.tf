6b4741d947f0d46d97271430adc581b5ff101431
Ezequiel Godoy <ezequiel.godoy@binbash.com.ar>
2025-03-26 20:03:38 +0100

    fix versions

--- apps-devstg/us-east-1/alb-logs-s3-bucket/config.tf
+++ apps-devstg/us-east-1/alb-logs-s3-bucket/config.tf
@@ -10,10 +10,10 @@ provider "aws" {
 # Backend Config (partial)    #
 #=============================#
 terraform {
-  required_version = "~> 1.5.0"
+  required_version = "~> 1.5"
 
   required_providers {
-    aws = "~> 4.10"
+    aws = "~> 5.0"
   }
 
   backend "s3" {
