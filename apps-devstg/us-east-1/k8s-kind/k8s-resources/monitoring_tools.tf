858dc8b0d3a04bca657862b17e1ddb98e504b1b2
arbb <>
2025-04-17 12:00:29 -0300

    Update goldilocks to v5

--- apps-devstg/us-east-1/k8s-kind/k8s-resources/monitoring_tools.tf
+++ apps-devstg/us-east-1/k8s-kind/k8s-resources/monitoring_tools.tf
@@ -20,7 +20,7 @@ resource "helm_release" "goldilocks" {
   namespace  = kubernetes_namespace.monitoring.id
   repository = "https://charts.fairwinds.com/stable"
   chart      = "goldilocks"
-  version    = "3.3.1"
+  version    = "5.0.0"
   values     = [file("chart-values/goldilocks.yaml")]
   depends_on = [helm_release.vpa]
 }
