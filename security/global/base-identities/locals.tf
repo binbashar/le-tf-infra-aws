7a9169b1c032453c7b21b34d9304787dc59e7f76
Diego OJ <diego.ojeda@binbash.com.ar>
2025-07-24 20:12:18 -0300

    Create separate user/group for Atlantis, propose a new approach for managing machine users PGP keys, and refactor machine users outputs

--- security/global/base-identities/locals.tf
+++ security/global/base-identities/locals.tf
@@ -17,9 +17,20 @@ locals {
     "franco.gauchat",
   ]
 
-  machine_users = [
-    "machine.circle.ci",
-    "machine.github.actions",
-    "machine.s3.demo"
-  ]
+  machine_users = {
+    # These machine users are managed by us, the DevOps/Infra team. For these
+    # we don't really need to create separate GPG keys per user because we will
+    # be ones decrypting the encrypted secret access key. So, a single key
+    # should suffice and simplify the process.
+    # Please continue reading the comment below.
+    "machine.circle.ci" = "machine.circle.ci"
+    "machine.github.actions" = "machine.github.actions"
+    "machine.s3.demo" = "machine.s3.demo"
+
+    # The machine users below follow the approach suggested by the comment
+    # above and so they start using one key for all machine users.
+    # Note: once all existing machine users have been migrated to this new
+    # approach, the code will need to be simplified and cleaned up.
+    "machine.atlantis" = "machine.infra"
+  }
 }
