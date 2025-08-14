5fedf428eb4fd8211ea23f7b912f54bad302ccfe
Luis M. Gallardo D. <lgallard@gmail.com>
2025-08-14 19:39:19 +0200

    chore(deps): update helm release uptime-kuma to v2.21.2 (clean) (#882)
    chore(deps): update helm release uptime-kuma to v2.21.2
    
    Clean update of uptime-kuma Helm chart from version 2.14.2 to 2.21.2.
    This resolves the Renovate PR #736 issue where manual changes caused conflicts.

--- apps-devstg/us-east-1/k8s-eks-demoapps/k8s-components/monitoring-other.tf
+++ apps-devstg/us-east-1/k8s-eks-demoapps/k8s-components/monitoring-other.tf
@@ -42,7 +42,7 @@ resource "helm_release" "uptime_kuma" {
   namespace  = kubernetes_namespace.monitoring_other[0].id
   repository = "https://helm.irsigler.cloud"
   chart      = "uptime-kuma"
-  version    = "2.14.2"
+  version    = "2.21.2"
   values = [
     <<-EOT
       ingress:
