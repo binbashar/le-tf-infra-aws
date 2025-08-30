4149709f907096e13cb6f1cd2f0948feaac24440
Diego OJ <diego.ojeda@binbash.com.ar>
2025-08-30 11:48:23 -0300

    Fix syntax

--- shared/us-east-1/tools-atlantis-ecs/main.tf
+++ shared/us-east-1/tools-atlantis-ecs/main.tf
@@ -56,7 +56,6 @@ module "atlantis" {
         value = join(",", ["github.com/binbashar/le-tf-infra-aws"]) # TODO
       },
       {
-      {
         name  = "ATLANTIS_REPO_CONFIG_JSON",
         value = jsonencode(yamldecode(file("${path.module}/server-atlantis.yaml"))),
       },
