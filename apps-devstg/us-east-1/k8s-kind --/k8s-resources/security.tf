44a74c5c45a4e2f9cf7e7eff5c2faea79f3cd7b6
Luis M. Gallardo D. <lgallard@gmail.com>
2025-11-11 20:20:00 +0100

    chore: remove cert-manager from helm-minors group (splitting to separate PR)
    Reverting cert-manager v1.18.2 → v1.19.0 update to split into a separate PR.
    
    Rationale:
    - cert-manager v1.19.0 contains a known certificate renewal bug
    - v1.19.1 (released Oct 15) fixes the bug + patches 9 CVEs
    - Splitting allows the other 4 stable helm charts to proceed
    - cert-manager will be upgraded directly to v1.19.1 in a separate PR
    
    Unblocked updates in this PR:
    - ingress-nginx
    - kubernetes-dashboard
    - node-exporter
    - fluent-bit
    - argocd-image-updater
    - cluster-autoscaler
    
    See PR comment for full analysis.

--- apps-devstg/us-east-1/k8s-kind --/k8s-resources/security.tf
+++ apps-devstg/us-east-1/k8s-kind --/k8s-resources/security.tf
@@ -7,7 +7,7 @@ resource "helm_release" "cert_manager" {
   namespace  = kubernetes_namespace.cert_manager.id
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = "v1.19.0"
+  version    = "v1.18.2"
   values = [
     templatefile("chart-values/certmanager.yaml",
       {
