9dafdcf383e8673f0c3e39b8bb533da55b4d2808
Rene Montilva <renemontilva@gmail.com>
2025-11-20 22:45:33 +0100

    feat(ISSUE-719): initialize baseline Terraform layer configuration for AWS backend
    - Added Terraform configuration files for managing S3 and DynamoDB resources for remote state storage and locking.
    - Introduced provider settings for AWS, including version constraints and backend configurations.
    - Created import blocks for existing resources to facilitate state management.
    - Established local variables and settings for account-specific configurations.
    - Included a README for setup instructions and a migration guide for adopting the new framework.
    
    This commit sets up the foundational infrastructure for managing Terraform state across multiple AWS accounts.

---
+++ baseline/base-tf-backend/main.tf
@@ -0,0 +1,40 @@
+#================================#
+# Terraform Backend Modules      #
+#================================#
+
+# Security Account Backend
+module "base_tf_backend" {
+  for_each = local.accounts_resources
+  providers = {
+    aws.primary   = aws.accounts["${each.key}-${each.value.region_primary}"]
+    aws.secondary = aws.accounts["${each.key}-${each.value.region_secondary}"]
+  }
+
+  source = "github.com/binbashar/terraform-aws-tfstate-backend.git?ref=v1.0.28"
+
+  delimiter = each.value.inputs.bucket.delimiter
+  namespace = each.value.inputs.bucket.namespace
+  stage     = each.key
+  name      = "terraform-backend"
+
+  acl                           = each.value.inputs.security.acl
+  block_public_acls             = each.value.inputs.security.block_public_acls
+  block_public_policy           = each.value.inputs.security.block_public_policy
+  restrict_public_buckets       = each.value.inputs.security.restrict_public_buckets
+  enable_server_side_encryption = each.value.inputs.security.enable_server_side_encryption
+  enforce_ssl_requests          = each.value.inputs.security.enforce_ssl_requests
+  ignore_public_acls            = each.value.inputs.security.ignore_public_acls
+  force_destroy                 = each.value.inputs.security.force_destroy
+
+  bucket_replication_enabled    = each.value.inputs.replication.bucket_replication_enabled
+  notifications_sns             = each.value.inputs.replication.notifications_sns
+  bucket_lifecycle_enabled      = each.value.inputs.replication.bucket_lifecycle_enabled
+  billing_mode                  = each.value.inputs.replication.billing_mode
+  enable_point_in_time_recovery = each.value.inputs.replication.enable_point_in_time_recovery
+  create_kms_key                = each.value.inputs.replication.create_kms_key
+
+  tags = merge(each.value.inputs.tags, {
+    Account   = each.key
+    AccountID = tostring(each.value.id)
+  })
+}
---
+++ baseline/base-tf-backend/variables.tf
@@ -0,0 +1,36 @@
+#================================#
+# Local variables                #
+#================================#
+variable "backend_settings" {
+  type = object({
+    # Bucket Name
+    bucket = object({
+      key       = string
+      name      = optional(string, "terraform-backend")
+      delimiter = optional(string, "-")
+      namespace = string
+    })
+    # Security
+    security = object({
+      acl                           = optional(string, "private")
+      block_public_acls             = optional(bool, true)
+      block_public_policy           = optional(bool, true)
+      restrict_public_buckets       = optional(bool, true)
+      enable_server_side_encryption = optional(bool, true)
+      enforce_ssl_requests          = optional(bool, true)
+      ignore_public_acls            = optional(bool, true)
+      force_destroy                 = optional(bool, false)
+    })
+    # Replication
+    replication = object({
+      bucket_replication_enabled    = optional(bool, true)
+      notifications_sns             = optional(bool, false)
+      bucket_lifecycle_enabled      = optional(bool, false)
+      billing_mode                  = optional(string, "PROVISIONED")
+      enable_point_in_time_recovery = optional(bool, false)
+      create_kms_key                = optional(bool, false)
+    })
+    # Tags
+    tags = map(string)
+  })
+}
