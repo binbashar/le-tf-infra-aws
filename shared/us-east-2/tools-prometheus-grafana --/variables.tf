d897828acd536a4c0f106f3554c2488a513772d3
Juan Matías Kungfoo de la Cámara Beovide <juan.delacamara@binbash.com.ar>
2026-01-23 11:30:23 -0300

    Added comment on what Ubuntu release to use (#1019)

--- shared/us-east-2/tools-prometheus-grafana --/variables.tf
+++ shared/us-east-2/tools-prometheus-grafana --/variables.tf
@@ -19,6 +19,13 @@ variable "name" {
 #
 # EC2 Attributes
 #
+# NOTE: when changing the OS version keep in mind:
+# - You can get the AMI ID from this page: https://cloud-images.ubuntu.com/locator/ec2/
+# - Then, using the ID, get the OS_ID (the aws_ami_os_id string) from here: https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#Images:visibility=public-images;v=3;$case=tags:false%5C,client:false;$regex=tags:false%5C,client:false
+#     - Note you should look in public images
+#     - Change the region to your desired one
+# - IMPORTANT: when using UBUNTU prioritize LTS (Long Term Support) releases to ensure system stability and extended security updates!
+#     - You can check releases here: https://en.wikipedia.org/wiki/Ubuntu_version_history
 variable "aws_ami_os_id" {
   description = "AWS AMI Operating System Identificator"
   default     = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"
