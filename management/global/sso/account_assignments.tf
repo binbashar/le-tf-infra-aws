8d696979f3886f7f7b69fc7e97e3281211c79ccc
Luis M. Gallardo D. <lgallard@gmail.com>
2026-07-27 16:24:21 +0200

    fix: grant Nestor Marketplace buyer access (#1129)

--- management/global/sso/account_assignments.tf
+++ management/global/sso/account_assignments.tf
@@ -249,6 +249,13 @@ module "account_assignments" {
       account             = var.accounts.apps-devstg.id
     },
     {
+      permission_set_arn  = module.permission_sets.permission_sets["MarketplaceBuyer"].arn
+      permission_set_name = "MarketplaceBuyer"
+      principal_type      = local.principal_type_group
+      principal_name      = local.groups["marketplacevalidationbuyers"].name
+      account             = var.accounts.data-science.id
+    },
+    {
       permission_set_arn  = module.permission_sets.permission_sets["MarketplaceAIPublisher"].arn
       permission_set_name = "MarketplaceAIPublisher"
       principal_type      = local.principal_type_group
